3c10679fd4880f4db317c58552f68aea9fe174c2
ErikHorvath-git <mailnavsetko206@gmail.com>
2022-11-10 17:06:04 +0100

    added 0-36numSystems conversion

--- Projekt/projekt.py
+++ Projekt/projekt.py
@@ -1,34 +1,51 @@
 """Basic converter from decimal numeral system."""
+# Converter for numeral systems starting from unary numeral system
+# and ending at the hexatridecimal numeral system.
+#
+# Other numeral systems are basically useless because there is no
+# usage for them and there is not enough letters in
+# the latin alphabet for them to use.
 
-# Dictionary with values for hexadecimal numeral system
-dict_with_letters = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+# Create a dictionary of the latin alphabet for numeral systems
+# that have the base bigger than 10 -> 10: 'A', 11: 'B' .....,  35: 'Z'
+keys = range(10, 36)
+values = [chr(i) for i in range(65, 91)]
+letters_dict = dict(zip(keys, values))
 
-# Ask the user from which numeral system to which
-# numeral system they want the number to be converted.
+# Ask the user to which numeral system they want the number to be converted.
 dest_base = int(input("Zadej číslem číselnou soustavu, "
                       "do které chceš číslo převést: "))
-# Ask user what the number they want to convert is.
-num = int(input(f"Zadej číslo v desítkové soustavě,"
-                f"které chceš převést do {dest_base} soustavy: "))
-num_copy = num
-mod_list = []
 
-while num > 0:
-    mod_list.append(num % dest_base)
-    num //= dest_base
+# This Converter can only covert numbers if the destination base is higher
+# than 0 or lower than 37.
+if 0 < dest_base < 37:
+    # Ask user what the number they want to convert is.
+    num = int(input(f"Zadej číslo v desítkové soustavě,"
+                    f"které chceš převést do {dest_base} soustavy: "))
+    num_copy = num
+    mod_list = []
 
-# If the destination numeral system is hexadecimal,
-# we need to cover the letter usage in this numeral system
+    while num > 0:
+        mod_list.append(num % dest_base)
+        num //= dest_base
 
-if dest_base == 16:
-    for i in range(len(mod_list)):
-        if mod_list[i] > 9:
-            mod_list[i] = dict_with_letters[mod_list[i]]
+    # If the destination numeral system is hexadecimal,
+    # we need to cover the letter usage in this numeral system
 
-mod_list.reverse()
-mod_list = [str(i) for i in mod_list]
-result = "".join(mod_list)
+    if dest_base > 10:
+        for i in range(len(mod_list)):
+            if mod_list[i] > 9:
+                mod_list[i] = letters_dict[mod_list[i]]
 
-print()
-print(f"Vámi zadané číslo {num_copy} z desítkové "
-      f"soustavy se v {dest_base} soustavě zapisuje jako {result}.")
+    mod_list.reverse()
+    mod_list = [str(i) for i in mod_list]
+    result = "".join(mod_list)
+
+    print()
+    print(f"Vámi zadané číslo {num_copy} z desítkové "
+          f"soustavy se v {dest_base} soustavě zapisuje jako {result}.")
+else:
+    print()
+    print("Převodce soustav nedokáže číslo převést mezi "
+          "Vámi zadanými soustavami, prosím zkuste to "
+          "znovu s jinými soustavami.")
