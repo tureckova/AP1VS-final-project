c2f8f251191f464f3296969c06713c13640dc5d8
konarikm <martinkonarik01@gmail.com>
2022-11-10 16:49:00 +0100

    Fixed converting to hexadecimal system

--- Projekt/projekt.py
+++ Projekt/projekt.py
@@ -1,7 +1,13 @@
 """Basic converter from decimal numeral system."""
 
+# Dictionary with values for hexadecimal numeral system
+dict_with_letters = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
+# Ask the user from which numeral system to which
+# numeral system they want the number to be converted.
 dest_base = int(input("Zadej číslem číselnou soustavu, "
                       "do které chceš číslo převést: "))
+# Ask user what the number they want to convert is.
 num = int(input(f"Zadej číslo v desítkové soustavě,"
                 f"které chceš převést do {dest_base} soustavy: "))
 num_copy = num
@@ -10,6 +16,14 @@ mod_list = []
 while num > 0:
     mod_list.append(num % dest_base)
     num //= dest_base
+
+# If the destination numeral system is hexadecimal,
+# we need to cover the letter usage in this numeral system
+
+if dest_base == 16:
+    for i in range(len(mod_list)):
+        if mod_list[i] > 9:
+            mod_list[i] = dict_with_letters[mod_list[i]]
 
 mod_list.reverse()
 mod_list = [str(i) for i in mod_list]
