ca5ea489f5c7535779661c04b866781728db6cf8
konarikm <martinkonarik01@gmail.com>
2022-11-24 09:01:26 +0100

    Add types to the variables

--- Projekt/code/ciselne_soustavy.py
+++ Projekt/code/ciselne_soustavy.py
@@ -7,17 +7,17 @@ Other numeral systems are basically useless because there is no
 usage for them and there is not enough letters in
 the latin alphabet for them to use.
 """
-from typing import List
+from typing import List, Dict
 
 # Create a dictionary of the latin alphabet for numeral systems
 # that have the base bigger than 10 -> 10: 'A', 11: 'B', .....,  35: 'Z'
 keys: List[int] = range(10, 36)
-values = [chr(i) for i in range(65, 91)]
-letters_dict = dict(zip(keys, values))
+values: List[str] = [chr(i) for i in range(65, 91)]
+letters_dict: Dict[int, str] = dict(zip(keys, values))
 
 
-def to_digits(number: int):
-    """Convert number to a list of digits of the number."""
+def to_digits(number: int) -> List[int]:
+    """Convert decimal number to a list of digits of the number."""
     digits: List[int] = []
 
     while number > 0:
@@ -28,12 +28,12 @@ def to_digits(number: int):
     return digits
 
 
-def to_decimal(source_base: int, number: int, digits: List[int]):
-    """Covert number from source_base to the number in decimal system."""
+def to_decimal(source_base: int, number: int, digits: List[int]) -> int:
+    """Convert number from source_base to the number in decimal system."""
     decimal_number: int = 0
 
     if not digits:
-        digits = to_digits(number)
+        digits: List[int] = to_digits(number)
 
     digits.reverse()
     for i in range(len(digits)):
@@ -42,7 +42,7 @@ def to_decimal(source_base: int, number: int, digits: List[int]):
     return decimal_number
 
 
-def to_destination_base(destination_base: int, dec_num: int):
+def to_destination_base(destination_base: int, dec_num: int) -> List[str]:
     """Convert number from decimal numeral system to number in dest_base."""
     mod_list = []
 
@@ -71,7 +71,7 @@ def to_destination_base(destination_base: int, dec_num: int):
     return mod_list
 
 
-def convert(source_base: int, destination_base: int, number: str) -> List[str]:
+def convert(source_base: int, destination_base: int, number: str) -> str:
     """Convert number from src_base to dest_base by using other functions."""
     # If the source base is not equal to the decimal numeral system,
     # we have to convert the number from source base
@@ -79,23 +79,23 @@ def convert(source_base: int, destination_base: int, number: str) -> List[str]:
     if source_base != 10:
         # Check if the number user inputted cointains only numbers
         if number.isdecimal():
-            dec_num = to_decimal(source_base, int(number), [])
+            dec_num: int = to_decimal(source_base, int(number), [])
 
         else:
             # Convert number with letters in it to list of digits
-            num_with_lett = [i for i in number]
+            num_with_lett: List[str] = [i for i in number]
             for i in range(len(num_with_lett)):
                 # If the item on current index is not a digit from 0 - 10,
                 # use the appropriate number from letters_dict
                 if not num_with_lett[i].isdecimal():
                     num_with_lett[i] = keys[values.index(num_with_lett[i])]
 
-            digits = [int(i) for i in num_with_lett]
+            digits: List[int] = [int(i) for i in num_with_lett]
             dec_num = to_decimal(source_base, 0, digits)
     else:
-        dec_num = int(number)
+        dec_num: int = int(number)
 
-    res = to_destination_base(destination_base, dec_num)
+    res: List[str] = to_destination_base(destination_base, dec_num)
     res = "".join(res)
     return res
 
@@ -104,24 +104,26 @@ def main():
     """Put the whole thing into main function so pytest works as intended."""
     # Ask the user from which numeral system to which
     # numeral system they want the number to be converted.
-    src_base = int(input("Zadej číslem číselnou soustavu, "
-                         "ze které chceš číslo převést: "))
+    src_base: int = int(input("Zadej číslem číselnou soustavu, "
+                              "ze které chceš číslo převést: "))
 
-    dest_base = int(input("Zadej číslem číselnou soustavu, "
-                          "do které chceš číslo převést: "))
+    dest_base: int = int(input("Zadej číslem číselnou soustavu, "
+                               "do které chceš číslo převést: "))
 
     # This Converter can only covert numbers if the destination base and
-    # source base is higher than 0 or lower than 37.
+    # source base is higher than 0 and lower than 37.
     if 0 < dest_base < 37 and 0 < src_base < 37:
         # If the condition of the source and destination base is met, ask user
         # what the number they want to convert is.
-        num = input(f"Zadej číslo které chceš převést z {src_base} soustavy "
-                    f"do {dest_base} soustavy: ")
-        original_number = num
+        num: str = input(f"Zadej číslo které chceš převést z {src_base}"
+                         f"soustavy do {dest_base} soustavy: ")
+        original_number: str = num
+
         if num == "0":
             result = 0
         else:
-            result = convert(src_base, dest_base, num)
+            result: str = convert(src_base, dest_base, num)
+
         print()
         print(f"Vámi zadané číslo {original_number} z {src_base} "
               f"soustavy se v {dest_base} soustavě zapisuje jako {result}.")
