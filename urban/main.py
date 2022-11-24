4b8b5c677f1ca03ffbe5984b84c93b90a0d1dc3b
Lukáš Urban <lukas.urbann@seznam.cz>
2022-11-24 15:29:57 +0100

    kuba is needy

--- urban/main.py
+++ urban/main.py
@@ -1,5 +1,8 @@
 import hashlib
 
+"""
+A command-line utility for viewing cryptographic translations of user defined passwords.
+"""
 def EncryptPassword(passwd, type):
     match type:
         case "md5":
@@ -14,6 +17,7 @@ def EncryptPassword(passwd, type):
             print("Funny non existent hash algorithm detected.")
             return
 
+    print("You've chosen " + type + " as your hash algorithm.")
     print("The hexadecimal value of this password is: " + result.hexdigest())
     print("Thanks, and don't forget to come again!")
 
@@ -25,8 +29,10 @@ def TypePassword():
 
 
 def ChooseAlgorithm():
-    print("Please, choose your algorithm:\n")
+    print("Please, choose your algorithm:\n[1 - SHA1 || 2 - SHA256 || 3 - SHA512 || 4 - MD5]\n")
     count = input()
+
+    typeos = ''
 
     match count:
         case "1":
@@ -37,18 +43,27 @@ def ChooseAlgorithm():
             typeos = "sha512"
         case "4":
             typeos = "md5"
+        case _:
+            typeos = ''
 
     return typeos
 
 
 def Intro():
+    """Prints the welcome string."""
     print("Welcome to the best password hasher 2022")
 
 
 if __name__ == '__main__':
     Intro()
-    passwd = TypePassword()
-    algorithm = ChooseAlgorithm()
-    print(passwd)
-    print(algorithm)
+
+    passwd = ''
+    algorithm = ''
+
+    while passwd == '':
+        passwd = TypePassword()
+
+    while algorithm == '':
+        algorithm = ChooseAlgorithm()
+
     EncryptPassword(passwd, algorithm)
