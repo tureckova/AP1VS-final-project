eb3b4ad9f1eb09445935b1c6f52d4e7e055c40b2
swpv <swpv@email.cz>
2022-11-30 14:30:36 +0100

    oprava kodu

--- prvocislo.py
+++ prvocislo.py
@@ -1,11 +1,13 @@
-x=input("ZAdaj cislo: ")
-y = 0
-for i in range(2,x):
-    if (x%i)==0:
-        y = 1
-        break
-if y == 1:
-    print(x, "nie je prvocislo")
-else:
-    print(x, "je prvocislo")
+def primeNumber(x):
+    y = 0
+    for i in range(2,x):
+        if (x%i)==0:
+            y = 1
+            break
+    if y == 1:
+        print(x, "nie je prvocislo")
+    else:
+        print(x, "je prvocislo")
+
+primeNumber(x=int(input("Zadaj cislo: ")))
 
