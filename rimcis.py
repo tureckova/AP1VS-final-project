8b757967dbddcead35526de8e106a0b685d60cd7
Daniel Štefka <d_stefka@ad.utb.cz>
2022-11-30 11:23:19 +0100

    vytvoreni funkce convert_num_to_rn

--- rimcis.py
+++ rimcis.py
@@ -5,6 +5,8 @@
 Následuje ukázka vzorové funkce.
 """
 
+rn_chars = ["M", "D", "C", "L", "X", "V", "I"]
+rn_maps_to = [1000, 500, 100, 50, 10, 5, 1]
 
 def compute(x):
     """Funkce počítá výsledek výrazu pro zadaný agrument x.
@@ -15,10 +17,41 @@ def compute(x):
     >>> compute(3)
     3
     """
-    x2 = x * x
-    return x2 - 2 * x
+    return 3
 
+
+def convert_num_to_rn(number_input):
+
+    i = 0
+    rn_result = ""
+
+    while i < len(rn_maps_to):
+
+        decreaser_index = 0
+        decreaser = 0
+
+        if i < len(rn_maps_to) - 1:
+
+            decreaser_index = i + (2 if i % 2 == 0 else 1)
+            decreaser = rn_maps_to[decreaser_index]
+
+        if number_input >= rn_maps_to[i]:
+
+            number_input -= rn_maps_to[i]
+            rn_result += rn_chars[i]
+
+        elif number_input >= rn_maps_to[i] - decreaser:
+
+            number_input -= rn_maps_to[i] - decreaser
+            rn_result += RNChars[decreaser_index] + rn_chars[i]
+
+        else:
+            i = i + 1
+
+    return rn_result
 
 if __name__ == '__main__':
+
     num = int(input('Insert a number:'))
-    print(compute(num))
+    print(convert_num_to_rn(num))
+
