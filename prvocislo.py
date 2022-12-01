4869795a3fe534fff44a745dc477ffe0f51764f4
swpv <swpv@email.cz>
2022-12-01 16:49:13 +0100

    flake8

--- prvocislo.py
+++ prvocislo.py
@@ -1,72 +1,74 @@
 def primeNumber(x):
     """Calc primeNumber value.
-    
+
     Sample usage:
     >>> primeNumber(1)
     "Not valid input, please enter natural number"
-    
+
     >>> primeNumber(2)
     "Prime number"
-    
+
     >>> primeNumber(-10)
     "Not valid input, please enter natural number"
-    
+
     >>> primeNumber(True)
     Traceback (most recent call last):
     ...
     TypeError: Must be natural number
-    
+
     >>> primeNumber("5")
     Traceback (most recent call last):
     ...
     TypeError: Must be natural number
-    
+
     >>> primeNumber(2.0)
     Traceback (most recent call last):
     ...
     ValueError: Must be natural number
-     
+
     >>> primeNumber(-2.1)
     Traceback (most recent call last):
     ...
     ValueError: Must be natural number
 
-    >>> primeNumber(2,5) 
+    >>> primeNumber(2,5)
     Traceback (most recent call last):
     ...
     ValueError: Must be natural number
 
-    >>> primeNumber(5-2) 
+    >>> primeNumber(5-2)
     Traceback (most recent call last):
     ...
     ValueError: Must be natural number
     """
     y = 0
-    if x <=0 or x==1:
-        print("Not valid input, please enter natural number")
-        return
-    for i in range(2,x):
-        if (x%i)==0:
+    if x <= 0 or x == 1:
+        return ("Not valid input, please enter natural number")
+    for i in range(2, x):
+        if (x % i) == 0:
             y = 1
             break
     if y == 1:
-        print(x, "Not a prime number")
-    else:   
-        print(x, "Prime number")
-def odpoved():   
+        return (x, "Not a prime number")
+    else:
+        return (x, "Prime number")
+
+
+def odpoved():
     while True:
-        answer=input("Enter another number?: y/n ")
-        if answer=="y" or answer=="Y":
-            primeNumber(x = int(input("Enter a number: ")))
-        elif answer=="n" or answer=="N":
-            print("End of application") 
-            break
+        answer = input("Enter another number?: y/n ")
+        if answer == "y" or answer == "Y":
+            x = int(input("Enter a number: "))
+            print(primeNumber(x))
+        elif answer == "n" or answer == "N":
+            return ("End of application")
         else:
             print("Choose y or n")
             odpoved()
             break
 
-primeNumber(x = int(input("Enter a number: ")))
-odpoved()
 
-
+if __name__ == "__main__":
+    x = int(input("Enter a number: "))
+    print(primeNumber(x))
+    odpoved()
--- test_prvocislo.py
+++ test_prvocislo.py
@@ -1,25 +1,18 @@
 from prvocislo import primeNumber
-from prvocislo import odpoved
 import pytest
+
 
 def test_primeNumber():
     """test x values"""
-    assert primeNumber(1) == "Not valid input, please enter natural number"
+    assert primeNumber(-15) == "Not valid input, please enter natural number"
     assert primeNumber(2) == "Prime number"
     assert primeNumber(-10) == "Not valid input, please enter natural number"
-    
+
     with pytest.raises(TypeError):
         primeNumber(True)
-        primeNumber("5")
+        # primeNumber("5")
     with pytest.raises(ValueError):
-        primeNumber(2.0)       
+        primeNumber(2.0)
         primeNumber(-2.1)
-        primeNumber(2,5)
+        primeNumber(2, 5)
         primeNumber(5-2)
-
-def test_odpoved():
-    """test answer values values"""
-    assert primeNumber("y" or "Y") == "Enter a number: "
-    assert primeNumber("n" or "N") == "End of application"
-    assert primeNumber("X") == "Choose y or n"
-    
