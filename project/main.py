1305e5871ef09481c5925e30374bed988279348e
swpv <swpv@email.cz>
2022-12-01 11:19:59 +0100

    file read

--- project/main.py
+++ project/main.py
@@ -7,7 +7,7 @@ def inputType():
     if argumentsNumber == 1:
         randomNumbers()
     elif argumentsNumber == 2:
-        print("document")
+        documentInput()
     elif argumentsNumber > 2:
         handleInputNumbers()
 
@@ -22,14 +22,23 @@ def handleInputNumbers():
 
 def randomNumbers():
     listNumbers = []
-    for x in range(21):
+    for x in range(20):
         listNumbers.append(random.randint(0, 50))
 
     return listNumbers
 
 
 def documentInput():
-    print()
+    with open(sys.argv[1]) as f:
+        contents = f.readline()
+
+    intCollection = []
+
+    for n in contents.split(" "):
+        intCollection.append(int(n))
+
+    print(intCollection)
+    return intCollection
 
 
 if __name__ == '__main__':
