ddc99a09d60be674d97efbbb147eec3555175fef
Matyáš Flekač <m_flekac@ad.utb.cz>
2022-12-01 13:31:33 +0100

    bubble sort and sort menu

--- project/main.py
+++ project/main.py
@@ -5,11 +5,11 @@ def inputType():
     argumentsNumber = len(sys.argv)
 
     if argumentsNumber == 1:
-        randomNumbers()
+        return sort(randomNumbers())
     elif argumentsNumber == 2:
-        documentInput()
+        return sort(documentInput())
     elif argumentsNumber > 2:
-        handleInputNumbers()
+        return sort(handleInputNumbers())
 
 
 def handleInputNumbers():
@@ -41,5 +41,32 @@ def documentInput():
     return intCollection
 
 
+def sort(numbers):
+    print("Select sort algorithm:")
+    print("1 - Quick sort")
+    print("2 - Insert sort")
+    print("3 - Bubble sort")
+
+    choice = input()
+
+    if choice == "1":
+        print("quick sort")
+    elif choice == "2":
+        print("insert sort")
+    elif choice == "3":
+        return bubbleSort(numbers)
+
+
+def bubbleSort(numbers):
+    for i in range(len(numbers)):
+        for j in range(0, len(numbers) - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                temp = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp
+
+    return numbers
+
+
 if __name__ == '__main__':
-    inputType()
+    print(inputType())
