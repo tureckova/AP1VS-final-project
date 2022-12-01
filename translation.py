6ee99c32018473e4d50f115a7267234d1e8fcb8f
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2022-12-01 13:28:44 +0100

    Flake8 fix

--- translation.py
+++ translation.py
@@ -1,36 +1,42 @@
+"""TODO: docs."""
 import googletrans
 from googletrans import Translator
 
 
 def destination_language():
+    """TODO: docs."""
     language = input('Do jakého jazyka si přejete překládat?')
-    while not(language in googletrans.LANGUAGES or language in googletrans.LANGUAGES.values()):
-        language = input('Do jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše!')
+    while not (language in googletrans.LANGUAGES or
+               language in googletrans.LANGUAGES.values()):
+        language = input("Do jakého jazyka si přejete překládat?"
+                         "Zadejte platný jazyk z listu výše!")
     return language
 
-#vypsani dostupnych jazyku z knihovny
-print('Tohle jsou všechny dostupné jazyky.')
+
+"""vypsani dostupnych jazyku z knihovny"""
+print("Tohle jsou všechny dostupné jazyky.")
 print(googletrans.LANGUAGES)
 translator = Translator()
 
-#input part
-srclan = input('Z jakého jazyka si přejete překládat? (nechte prázdné pro automatický překlad)')
+"""input part"""
+srclan = input("Z jakého jazyka si přejete překládat?"
+               "(nechte prázdné pro automatický překlad)")
 destlan = destination_language()
-text = input('Co je text který chcete přeložit?: ')
+text = input("Co je text který chcete přeložit?: ")
 
-#language detection
+"""language detection"""
 dt1 = translator.detect(text)
 print(dt1)
 
-#automaticky preklad
+"""automaticky preklad"""
 if srclan == '':
     result = translator.translate(text, dest=destlan)
 else:
-    #osetreni vstupu
+    """osetreni vstupu"""
     if srclan not in googletrans.LANGUAGES:
-        srclan = input('Z jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše! (nechte prázdné pro automatický překlad)')
-    if destlan not in googletrans.LANGUAGES:
-        destlan = input('Do jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše!')
+        srclan = input("Z jakého jazyka si přejete překládat?"
+                       "Zadejte platný jazyk z listu výše!"
+                       "(nechte prázdné pro automatický překlad)")
 
     result = translator.translate(text, src=srclan, dest=destlan)
 
