47c20efd7fc6c548a40159d0ed6ae0aa748cea75
Hspsr <119581613+Hspsr@users.noreply.github.com>
2022-12-01 12:13:54 +0100

    fixed issue with automatic translation

--- translation.py
+++ translation.py
@@ -1,25 +1,26 @@
 import googletrans
 from googletrans import Translator
-
+#vypsani dostupnych jazyku z knihovny
 print('Tohle jsou všechny dostupné jazyky.')
 print(googletrans.LANGUAGES)
 translator = Translator()
-
+#input part
 srclan = input('Z jakého jazyka si přejete překládat? (nechte prázdné pro automatický překlad)')
 destlan = input('Do jakého jazyka si přejete překládat?')
 text = input('Co je text který chcete přeložit?: ')
-
+#language detection
 dt1 = translator.detect(text)
 print(dt1)
-
-if srclan not in googletrans.LANGUAGES:
-    srclan = input('Z jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše! (nechte prázdné pro automatický překlad)')
-if destlan not in googletrans.LANGUAGES:
-    destlan = input('Do jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše!')
-
+#automaticky preklad
 if srclan == '':
     result = translator.translate(text, dest=destlan)
 else:
+    #osetreni vstupu
+    if srclan not in googletrans.LANGUAGES:
+        srclan = input('Z jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše! (nechte prázdné pro automatický překlad)')
+    if destlan not in googletrans.LANGUAGES:
+        destlan = input('Do jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše!')
+    
     result = translator.translate(text, src=srclan, dest=destlan)
     
 print(result.text)
