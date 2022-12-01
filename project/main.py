a5c8e0d7c293d8b268af42b5276de12f7bfae0f1
swpv <swpv@email.cz>
2022-12-01 10:41:16 +0100

    basic logic

--- project/main.py
+++ project/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def inputType(name):
+    if sys.argv == 0:
+        print("random")
+    elif sys.argv == 1:
+        print("document")
+    elif sys.argv > 1:
+        print("numbers")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
