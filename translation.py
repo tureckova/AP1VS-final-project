81a5a2697960c85f075a3530c13f165965826387
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2022-12-01 13:18:49 +0100

    Moved destination language to function

--- translation.py
+++ translation.py
@@ -1,21 +1,29 @@
 import googletrans
 from googletrans import Translator
+
+
+def destination_language():
+    language = input('Do jakého jazyka si přejete překládat?')
+    while not(language in googletrans.LANGUAGES or language in googletrans.LANGUAGES.values()):
+        language = input('Do jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše!')
+    return language
+
 #vypsani dostupnych jazyku z knihovny
 print('Tohle jsou všechny dostupné jazyky.')
 print(googletrans.LANGUAGES)
 translator = Translator()
+
 #input part
 srclan = input('Z jakého jazyka si přejete překládat? (nechte prázdné pro automatický překlad)')
-destlan = input('Do jakého jazyka si přejete překládat?')
+destlan = destination_language()
 text = input('Co je text který chcete přeložit?: ')
+
 #language detection
 dt1 = translator.detect(text)
 print(dt1)
+
 #automaticky preklad
 if srclan == '':
-    #osetreni vstupu
-    if destlan not in googletrans.LANGUAGES:
-        destlan = input('Do jakého jazyka si přejete překládat? Zadejte platný jazyk z listu výše!')
     result = translator.translate(text, dest=destlan)
 else:
     #osetreni vstupu
