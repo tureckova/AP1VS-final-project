defc5df7ed1a90a41cd6e13d21605b358c83971c
Filip Hajduch <filip.hajduch03@gmail.com>
2022-12-02 21:11:28 +0100

    pokus o opravu projektu main.py

--- test_main.py
+++ test_main.py
@@ -1,15 +1,13 @@
 import pytest
-from triangle import triangle
+from main import triangle
 
 def test_triangle():
     # return relu
-    assert triangle(1) == 1
-    assert triangle(-5) == 0
-    assert triangle(2.2) == 2.2
-    assert triangle(-2.2) == 0
+    assert triangle(1, 2, 3, 4, 5, 6) == 1
 
     # give typeError
     with pytest.raises(TypeError):
         triangle(1+5j)
         triangle(True)
         triangle("s")
+
