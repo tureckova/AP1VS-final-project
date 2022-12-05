ef73b41544169a4674bb511585fac34b8ecffa76
AlanekDo <118170797+AlanekDo@users.noreply.github.com>
2022-12-05 23:03:30 +0100

    Algortimus jak zjistit tisic

--- Projekt_RimskaCislice.py
+++ Projekt_RimskaCislice.py
@@ -11,5 +11,16 @@ Seznam zakladni rimskych cisel.
 I	V	X	L	C	D	M
 1	5	10	50	100	500	1000
 """
-pls aspon znamku nebo D
 
+
+def tisice(cislo):
+    """Zjisteni tisice.
+
+    >>> tisice(3888)
+    'MMM'
+    """
+    t = ["", "M", "MM", "MMM"]  # index v poli zacina s hodnotou 0
+    if isinstance(cislo, str) or isinstance(cislo, float):
+        raise TypeError("Nelze prevodit")
+    tisic = t[cislo // 1000]  # 3888 // 1000 == 3
+    return tisic  # znak na (3+1) pozici
