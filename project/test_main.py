0836105372876025c17fc615134b596e8b7f4aea
zeman.fi6 <zeman.fi6@gmail.com>
2022-12-05 19:30:23 +0100

    revert

--- project/test_main.py
+++ project/test_main.py
@@ -1,25 +1,32 @@
 """Tests for main.py."""
 import pytest
 import builtins
-import mainFile # noqa: F403
+from mainFile import minMax
+from mainFile import bubbleSort
+from mainFile import insertionSort
+from mainFile import quickSort
+from mainFile import documentInput
+from mainFile import randomNumbers
+from mainFile import handleInputNumbers
+from mainFile import sort
 
 
 def test_minMax():
     """Testing minmax function."""
-    assert mainFile.minMax([5, 4, 7, 6, 2]) == (7, 2)  # noqa: F405
-    assert mainFile.minMax([5, 2, 5, 1, 0]) == (5, 0)  # noqa: F405
-    assert mainFile.minMax([-5, -2, -5, -1, 0]) == (0, -5)  # noqa: F405
-    assert mainFile.minMax([5, -4, 7, -6, -2]) == (7, -6)  # noqa: F405
+    assert minMax([5, 4, 7, 6, 2]) == (7, 2)
+    assert minMax([5, 2, 5, 1, 0]) == (5, 0)
+    assert minMax([-5, -2, -5, -1, 0]) == (0, -5)
+    assert minMax([5, -4, 7, -6, -2]) == (7, -6)
 
     with pytest.raises(TypeError):
-        mainFile.minMax(["fgdf", 12, True, 5.4])  # noqa: F405
-        mainFile.minMax([12, 2.4, 4, 8])  # noqa: F405
+        minMax(["fgdf", 12, True, 5.4])
+        minMax([12, 2.4, 4, 8])
 
 
 def test_sort():
     """Testing sorting selection."""
     builtins.input = lambda: "1"
-    result = mainFile.sort([4, 2, 6, 5, 8])  # noqa: F405
+    result = sort([4, 2, 6, 5, 8])
     assert isinstance(result, list)
 
     for x in range(5):
@@ -28,65 +35,61 @@ def test_sort():
 
 def test_bubbleSort():
     """Testing bubble sort algorithm."""
-    assert mainFile.bubbleSort([4, 2, 6, 5, 8]) == [2, 4, 5, 6, 8]  # noqa: F405
-    assert mainFile.bubbleSort([5, 0, 4, 6, 12]) == [0, 4, 5, 6, 12]  # noqa: F405
-    assert mainFile.bubbleSort([-4, -2, -6, -5, -8]) ==\
-        [-8, -6, -5, -4, -2]  # noqa: F405
-    assert mainFile.bubbleSort([-5, 0, 4, -6, 12]) == [-6, -5, 0, 4, 12]  # noqa: F405
+    assert bubbleSort([4, 2, 6, 5, 8]) == [2, 4, 5, 6, 8]
+    assert bubbleSort([5, 0, 4, 6, 12]) == [0, 4, 5, 6, 12]
+    assert bubbleSort([-4, -2, -6, -5, -8]) == [-8, -6, -5, -4, -2]
+    assert bubbleSort([-5, 0, 4, -6, 12]) == [-6, -5, 0, 4, 12]
 
     with pytest.raises(TypeError):
-        mainFile.bubbleSort(["dfgh", 0, False, -6, 12.4])  # noqa: F405
-        mainFile.bubbleSort([15.2, 0, 5, 4, -4.1])  # noqa: F405
+        bubbleSort(["dfgh", 0, False, -6, 12.4])
+        bubbleSort([15.2, 0, 5, 4, -4.1])
 
 
 def test_insertionSort():
     """Testing insertion sort algorithm."""
-    assert mainFile.insertionSort([4, 2, 6, 5, 8]) == [2, 4, 5, 6, 8]  # noqa: F405
-    assert mainFile.insertionSort([5, 0, 4, 6, 12]) == [0, 4, 5, 6, 12]  # noqa: F405
-    assert mainFile.insertionSort([-4, -2, -6, -5, -8]) ==\
-        [-8, -6, -5, -4, -2]  # noqa: F405
-    assert mainFile.insertionSort([-5, 0, 4, -6, 12]) ==\
-        [-6, -5, 0, 4, 12]  # noqa: F405
+    assert insertionSort([4, 2, 6, 5, 8]) == [2, 4, 5, 6, 8]
+    assert insertionSort([5, 0, 4, 6, 12]) == [0, 4, 5, 6, 12]
+    assert insertionSort([-4, -2, -6, -5, -8]) == [-8, -6, -5, -4, -2]
+    assert insertionSort([-5, 0, 4, -6, 12]) == [-6, -5, 0, 4, 12]
 
     with pytest.raises(TypeError):
-        mainFile.insertionSort(["dfgh", 0, False, -6, 12.4])  # noqa: F405
-        mainFile.insertionSort([15.2, 0, 5, 4, -4.1])  # noqa: F405
+        insertionSort(["dfgh", 0, False, -6, 12.4])
+        insertionSort([15.2, 0, 5, 4, -4.1])
 
 
 def test_quickSort():
     """Testing quick sort algorithm."""
-    assert mainFile.quickSort([4, 2, 6, 5, 8]) == [2, 4, 5, 6, 8]  # noqa: F405
-    assert mainFile.quickSort([5, 0, 4, 6, 12]) == [0, 4, 5, 6, 12]  # noqa: F405
-    assert mainFile.quickSort([-4, -2, -6, -5, -8]) ==\
-        [-8, -6, -5, -4, -2]  # noqa: F405
-    assert mainFile.quickSort([-5, 0, 4, -6, 12]) == [-6, -5, 0, 4, 12]  # noqa: F405
+    assert quickSort([4, 2, 6, 5, 8]) == [2, 4, 5, 6, 8]
+    assert quickSort([5, 0, 4, 6, 12]) == [0, 4, 5, 6, 12]
+    assert quickSort([-4, -2, -6, -5, -8]) == [-8, -6, -5, -4, -2]
+    assert quickSort([-5, 0, 4, -6, 12]) == [-6, -5, 0, 4, 12]
 
     with pytest.raises(TypeError):
-        mainFile.quickSort(["dfgh", 0, False, -6, 12.4])  # noqa: F405
-        mainFile.quickSort([15.2, 0, 5, 4, -4.1])  # noqa: F405
+        quickSort(["dfgh", 0, False, -6, 12.4])
+        quickSort([15.2, 0, 5, 4, -4.1])
 
 
 def test_documentInput():
     """Testing document input."""
-    assert mainFile.documentInput("project/testfiles/testfile.txt") ==\
-           [5, 4, 7, 8, 9, 3, -4, 0, 1, 2]  # noqa: F405
-    assert mainFile.documentInput("project/testfiles/testfile2.txt") ==\
-           [5, 8, -4, 0, -2, -3, 1]  # noqa: F405
+    assert documentInput("project/testfiles/testfile.txt") ==\
+           [5, 4, 7, 8, 9, 3, -4, 0, 1, 2]
+    assert documentInput("project/testfiles/testfile2.txt") ==\
+           [5, 8, -4, 0, -2, -3, 1]
 
     with pytest.raises(ValueError):
-        assert mainFile.documentInput("project/testfiles/testfile3.txt")  # noqa: F405
+        assert documentInput("project/testfiles/testfile3.txt")
 
 
 def test_randomNumbers():
     """Testing that random numbers are numbers."""
-    assert isinstance(mainFile.randomNumbers(), list)  # noqa: F405
-    assert isinstance(mainFile.randomNumbers()[2], int)  # noqa: F405
+    assert isinstance(randomNumbers(), list)
+    assert isinstance(randomNumbers()[2], int)
 
 
 def test_handleInputNumbers():
     """Testing that function returns numbers."""
     numbers = [4, 4, 6, -9]
-    assert isinstance(mainFile.handleInputNumbers(numbers), list)  # noqa: F405
+    assert isinstance(handleInputNumbers(numbers), list)
 
     for x in numbers:
         assert isinstance(x, int)
