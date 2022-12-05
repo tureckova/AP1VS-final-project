0fbcf111c865fa1acabd73e060f4b32c5d46a2f6
Kejblix <steam54847@gmail.com>
2022-12-05 19:58:27 +0100

    test

--- example.py
+++ example.py
@@ -1,30 +1,30 @@
-ENGLISH_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+CZECH_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
                     'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
                     'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
                     'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
                     '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
 
-# Generate MORSE_TO_ENGLISH from ENGLISH_TO_MORSE
-MORSE_TO_ENGLISH = {}
-for key, value in ENGLISH_TO_MORSE.items():
-    MORSE_TO_ENGLISH[value] = key
+# Generate MORSE_TO_CZECH from CZECH_TO_MORSE
+MORSE_TO_CZECH = {}
+for key, value in CZECH_TO_MORSE.items():
+    MORSE_TO_CZECH[value] = key
 
 
-def english_to_morse(message):
+def czech_to_morse(message):
     morse = []  # Will contain Morse versions of letters
     for char in message:
-        if char in ENGLISH_TO_MORSE:
-            morse.append(ENGLISH_TO_MORSE[char])
+        if char in CZECH_TO_MORSE:
+            morse.append(CZECH_TO_MORSE[char])
     return " ".join(morse)
 
 
-def morse_to_english(message):
+def morse_to_czceh(message):
     message = message.split(" ")
-    english = []  # Will contain English versions of letters
+    czech = []  # Will contain English versions of letters
     for code in message:
-        if code in MORSE_TO_ENGLISH:
-            english.append(MORSE_TO_ENGLISH[code])
-    return " ".join(english)
+        if code in MORSE_TO_CZECH:
+            czech.append(MORSE_TO_CZECH[code])
+    return " ".join(czech)
 
 
 def main():
@@ -36,14 +36,14 @@ def main():
     if response == "1":
         print("Enter Morse code (with a space after each code): ")
         morse = input("> ")
-        english = morse_to_english(morse)
+        czech = morse_to_czceh(morse)
         print("### English version ###")
-        print(english)
+        print(czech)
 
     elif response == "2":
         print("Zadej English text: ")
-        english = input("> ").upper()
-        morse = english_to_morse(english)
+        czech = input("> ").upper()
+        morse = czech_to_morse(czech)
         print("### Morse Code version ###")
         print(morse)
 
