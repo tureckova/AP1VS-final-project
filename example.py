fb16f8724623c9ad592a9cb35c3768ef6dfa7e3d
David Polastik <david.polastik@seznam.cz>
2022-12-06 18:19:56 +0100

    uprava

--- example.py
+++ example.py
@@ -1,52 +1,42 @@
-CZECH_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-                    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
-                    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-                    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
-                    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
+dictionary={'A':'.-', 'B':'-...',
+            'C':'-.-.', 'D':'-..', 'E':'.','F':'..-.', 'G':'--.', 'H':'....',
+            'I':'..', 'J':'.---', 'K':'-.-','L':'.-..', 'M':'--', 'N':'-.',
+            'O':'---', 'P':'.--.', 'Q':'--.-','R':'.-.', 'S':'...', 'T':'-',
+            'U':'..-', 'V':'...-', 'W':'.--','X':'-..-', 'Y':'-.--', 'Z':'--..',}
 
-# Generate MORSE_TO_CZECH from CZECH_TO_MORSE
-MORSE_TO_CZECH = {}
-for key, value in CZECH_TO_MORSE.items():
-    MORSE_TO_CZECH[value] = key
-
-
-def czech_to_morse(message):
-    morse = []  # Will contain Morse versions of letters
-    for char in message:
-        if char in CZECH_TO_MORSE:
-            morse.append(CZECH_TO_MORSE[char])
-    return " ".join(morse)
+dictionary2={'.-':'A','-...':'B','-.-.':'C','-..':'D', '.':'E','..-.':'F','--.':'G','....':'H',
+             '..':'I','.---':'J', '-.-':'K','.-..':'L', '--':'M', '-.':'N',
+             '---':'O', '.--.':'P', '--.-':'Q','.-.':'R', '...':'S', '-':'T',
+             '..-':'U', '...-':'V', '.--':'W','-..-':'X', '-.--':'Y', '--..':'Z',}
 
 
-def morse_to_czceh(message):
-    message = message.split(" ")
-    czech = []  # Will contain Czech versions of letters 
-    for code in message:
-        if code in MORSE_TO_CZECH:
-            czech.append(MORSE_TO_CZECH[code])
-    return " ".join(czech)
+for i in range(500):
+    d=int(input("1-zašifrovat 2=odšifrovat"))
+    a=input("Zadaj vec na šifrovanie")
+    a=a.upper()
+
+    sifra=""
 
 
-def main():
-    while True:
-        response = input("Convert Morse to Czech (1) or Czech to Morse (2)? ").upper()
-        if response == "1" or response == "2":
-            break
-
-    if response == "1":
-        print("Enter Morse code (with a space after each code): ")
-        morse = input("> ")
-        czech = morse_to_czceh(morse)
-        print("### Czech version ###")
-        print(czech)
-
-    elif response == "2":
-        print("Zadej Czech text: ")
-        czech = input("> ").upper()
-        morse = czech_to_morse(czech)
-        print("### Morse Code version ###")
-        print(morse)
 
 
-if __name__ == "__main__":
-    main()
+    if d==1: # šifrování
+
+        void=" "
+        for i in a: #indexování
+            if i != ' ':
+                void+=dictionary[i]+" "
+            else:
+
+                void += ' '
+
+
+        print(void)
+
+
+
+    else: # dešifrování
+        void = ""
+        splitstring = a.split(" ") #rozdělení
+        for i in splitstring: #indexování
+            void += dictionary2[i]
