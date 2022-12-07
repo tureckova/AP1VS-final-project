c48282fa823c8bf4c4611cba18c0e8020edf0054
hjakub <j_hrabec@utb.cz>
2022-12-07 21:31:58 +0100

    behem cisteni kodu jsem rozbil unit test; opraveno

--- test_number_systems.py
+++ test_number_systems.py
@@ -6,10 +6,10 @@ import pytest
 def test_number_system_converter():
     """Test the converter function."""
     # x > 0
-    assert number_system_converter(5) == 'Binary: 0b101, hexadecimal: 0x5, octal: 0o5'
-    assert number_system_converter(1) == 'Binary: 0b1, hexadecimal: 0x1, octal: 0o1'
-    assert number_system_converter(420) == 'Binary: 0b110100100, hexadecimal: 0x1a4, octal: 0o644'
-    assert number_system_converter(123456789) == 'Binary: 0b111010110111100110100010101, hexadecimal: 0x75bcd15, octal: 0o726746425'
+    assert number_system_converter(5) == 'bin: 0b101, hex: 0x5, oct: 0o5'
+    assert number_system_converter(1) == 'bin: 0b1, hex: 0x1, oct: 0o1'
+    assert number_system_converter(420) == 'bin: 0b110100100, hex: 0x1a4, oct: 0o644'
+    assert number_system_converter(123456789) == 'bin: 0b111010110111100110100010101, hex: 0x75bcd15, oct: 0o726746425'
     # x <= 0
     with pytest.raises(ValueError):
         number_system_converter(0)
