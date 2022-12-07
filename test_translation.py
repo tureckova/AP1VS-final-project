d7a4b815a1a3bb33930760cf95650e0f6876d838
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2022-12-07 17:19:28 +0100

    Added more unittests and refactoring

--- test_translation.py
+++ test_translation.py
@@ -1,12 +1,27 @@
 """Test module translation."""
-from translation import translate
+from translation import translate, detect_language, is_valid_language
 
 
 def test_translate():
-    """Testing answers."""
-    # test case destination language
+    """Test for translation."""
     assert translate("Hellow world.", "cs") == "Ahoj světe."
     assert translate("Hellow world.", "e1m1") == "Neplatný jazyk výstupu."
-    assert translate("Hellow world.", "cs", "e1m1") == "Neplatný jazyk výstupu." # noqa
+    assert translate("Hellow world.", "cs", "e1m1") == "Neplatný jazyk výstupu."  # noqa
     assert translate("Das war ein Befehl!", "en", "de") == "This was an order!"
     assert translate("Amogus is great.", "cs", "auto") == "Amogus je skvělý."
+
+
+def test_detect_language():
+    """Test for detecting language."""
+    assert detect_language("ahoj") == "Detekovaný jazyk: cs"
+    assert detect_language("1") == "Detekovaný jazyk: en"
+    assert detect_language(1235) == "Text musí být nenulový string."
+    assert detect_language("     ") == "Text musí být nenulový string."
+    assert detect_language("") == "Text musí být nenulový string."
+
+
+def test_is_valid_language():
+    """Test for checking if language is valid."""
+    assert is_valid_language("cs")
+    assert not is_valid_language("123")
+    assert not is_valid_language("")
--- translation.py
+++ translation.py
@@ -8,6 +8,11 @@ translator = Translator()
 def translate(text, dest, src="auto"):
     """Translate given text.
 
+    :param text:
+    :param dest:
+    :param src:
+    :return:
+
     >>> translate("Hello world.", "cs")
     'Ahoj světe.'
     >>> translate("Hello world.", "e1m1")
@@ -22,11 +27,36 @@ def translate(text, dest, src="auto"):
     return translator.translate(text, src=src, dest=dest).text
 
 
-def get_source_language():  # pragma: no cover - TODO
+def detect_language(text):
+    """TODO.
+
+    :param text:
+    :return:
+    """
+    if type(text) is not str or not text.strip():
+        return "Text musí být nenulový string."
+    language = translator.detect(text)
+    return f"Detekovaný jazyk: {language.lang}"
+
+
+def is_valid_language(language):
+    """TODO.
+
+    :param language:
+    :return:
+    """
+    if language not in languages:
+        print("Neplatný jazyk.")
+        return False
+    return True
+
+
+def get_source_language():  # pragma: no cover
     """Get source language.
 
     Receive source language from which to translate
-    TODO: unit test
+
+    :return:
     """
     language = input("Z jakého jazyka si přejete překládat? "
                      "(nechte prázdné pro automatický překlad)")
@@ -39,38 +69,28 @@ def get_source_language():  # pragma: no cover - TODO
     return language
 
 
-def get_destination_language():  # pragma: no cover - TODO
-    """Get destination language.
-
-    Receive destination language to which to translate
-
-    TODO: unit test
-    """
-    language = input("Do jakého jazyka si přejete překládat?")
-    while language not in languages:
-        language = input("Do jakého jazyka si přejete překládat? "
-                         "Zadejte platný jazyk z listu výše!")
-    return language
-
-
 def main():
     """Entry point when run as script."""
     # vypsani dostupnych jazyku z knihovny
-    print("Tohle jsou všechny dostupné jazyky.")
-    print(LANGUAGES)
+    print("Tohle jsou všechny dostupné jazyky:"
+          f"\n{LANGUAGES}")
 
-    # input part
+    # set source language
     srclan = get_source_language()
-    destlan = get_destination_language()
-    srctext = input("Co je text který chcete přeložit?: ")
+    # set destination language
+    while not is_valid_language(destlan := input("Do jakého jazyka si přejete překládat?")):  # noqa
+        pass
+
+    while type(srctext := input("Co je text který chcete přeložit?: ")) is not str or not srctext.strip():  # noqa
+        print("Text musí být nenulový string.")
 
     # language detection
     if srclan == "auto":
-        print(translator.detect(srctext))
+        print(detect_language(srctext))
 
     print(translate(srctext, destlan, srclan))
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     """Executed if run as script."""
     main()
