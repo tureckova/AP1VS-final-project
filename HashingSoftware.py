fc39fc24aec7f65c03d34e6d2fe1b5e85fd2e5a3
swpv <swpv@email.cz>
2022-12-07 11:05:32 +0100

    added input validation

--- HashingSoftware.py
+++ HashingSoftware.py
@@ -12,9 +12,27 @@ def AlgorithmChoice(x):
         hashed = hashlib.sha512(hashIt.encode())
         return hashed.hexdigest()
 
-password = input("Password to hash: ")
-hash = input("Your own hash: ")
-x = input("Choose algorith (1 = md5 ; 2 = sha384 ; 3 = sha512): ")
+def CheckInput():
+    try:
+        testInput = int(userInput)
+        if testInput != 1 and testInput != 2 and testInput != 3:
+            raise ValueError("Choose number 1, 2 or 3!!")
+    except:
+        raise TypeError("The input must be integer!!")
+
+def CheckLength():
+    if len(password) < 8:
+        raise ValueError("Password should have 8 symbols or more!!")
+    if len(hash) < 5:
+        raise ValueError("Hash should have 5 symbols or more!!")
+
+
+password = input("Password to hash (8 or more symbols): ")
+hash = input("Your own hash (5 or more symbols): ")
+userInput = input("Choose algorith (1 = md5 ; 2 = sha384 ; 3 = sha512): ")
+
+CheckInput()
+CheckLength()
 
 print("-----------------------------------------------------------------------------------------------------------------------")
-print("Hashed password: " + AlgorithmChoice(int(x)) + "\n")
+print("Hashed password: " + AlgorithmChoice(int(userInput)) + "\n")
