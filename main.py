ebfe75dc48f1ef14cf2edee2d908e68df828fea7
Josef Kužel <119503297+eeagle9@users.noreply.github.com>
2022-12-07 12:20:55 +0100

    Update main.py
    Okomentoval jsem kod :)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ﻿import numpy
 
+#nadefinování proměnných, výpis textu na konzoli
 aX = float(input("Enter the x coordinate for point A: "))
 aY = float(input("Enter the y coordinate for point A: "))
 bX = float(input("Enter the x coordinate for point B: "))
@@ -7,6 +8,7 @@ bY = float(input("Enter the y coordinate for point B: "))
 cX = float(input("Enter the x coordinate for point C: "))
 cY = float(input("Enter the y coordinate for point C: "))
 
+#výpis hodnot na konzoli (hodnoty, které byly zadány uživatelem)
 print()
 print("Bod A = [%.1f, %.1f]" %(aX, aY))
 print("Bod B = [%.1f, %.1f]" %(bX, bY))
@@ -14,34 +16,45 @@ print("Bod C = [%.1f, %.1f]" %(cX, cY))
 
 time.sleep(2)#waitTime 2 sekundy
 
+#výpis textu
 print( )
 print("Výpočet délky jednotlivých stran trojúhelníku ABC:")
+#početní operace pro každou stranu (A,B,C)
 stranaA = numpy.sqrt(pow(bX - aX, 2) + pow(bY - aY, 2))
 stranaB = numpy.sqrt(pow(cX - bX, 2) + pow(cY - bY, 2))
 stranaC = numpy.sqrt(pow(aX - cX, 2) + pow(aY - cY, 2))
+#výpis vypočtených délek stran
 print("Délka strany a = %.1f cm." %stranaA)
 print("Délka strany b = %.1f cm." %stranaB)
 print("Délka strany c = %.1f cm." %stranaC)
 
 time.sleep(2)#waitTime 2 sekundy
 
+#výpis textu
 print( )
 print("Výpočet obvodu trojúhelníku ABC:")
+#výpočet obvodu Trojúhelníku
 obvod = stranaA + stranaB + stranaC
+#výpis výsledku
 print("Trojúhelník ABC má obvod = %.1f cm." %obvod)
 
 time.sleep(2)#waitTime 2 sekundy
 
+#výpis textu
 print( )
 print("Výpočet obsahu trojúhelníku ABC:")
+#výpočet obsahu Trojúhelníku
 s = (stranaA + stranaB + stranaC) / 2
 obsah = numpy.sqrt(s * (s - stranaA) * (s - stranaB) * (s - stranaC))
+#výpis výsledku
 print("Trojúhelník ABC má obsah = %.1f cm2." %obsah)
 
 time.sleep(2)#waitTime 2 sekundy
 
+#výpis textu
 print( )
 print("Ověření sestrojitelnosti trojúhelníku ABC:")
+#podmínka IF, ověření zda-li jde trojúhelník ABC sestrojit
 if ((stranaA + stranaB > stranaC) and
     (stranaB + stranaC > stranaA) and
     (stranaC + stranaA > stranaB)):
@@ -51,8 +64,10 @@ else:
 
 time.sleep(2)#waitTime 2 sekundy
     
+#výpis textu
 print( )
 print("Ověření pravoúhlosti trojúhelníku ABC:")
+#podmínka IF, ověření pravoúhlosti trojúhelníku ABC
 if ((pow(stranaA, 2) + pow(stranaB, 2) == pow(stranaC, 2)) or
     (pow(stranaB, 2) + pow(stranaC, 2) == pow(stranaA, 2)) or
     (pow(stranaC, 2) + pow(stranaA, 2) == pow(stranaB, 2))):
