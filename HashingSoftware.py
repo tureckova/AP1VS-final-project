fc1c01bbe34e294c9e934d9b3a268b5cd8f2d957
Filip Chromečka <f_chromecka@ad.utb.cz>
2022-12-07 11:29:11 +0100

    function rework

--- HashingSoftware.py
+++ HashingSoftware.py
@@ -1,7 +1,10 @@
+"""Cryptographic password hashing."""
+
+
 import hashlib
 
-def AlgorithmChoice(x):
-    hashIt = password + hash
+def AlgorithmChoice(x, passw, hsh):
+    hashIt = passw + hsh
     if x == 1:
         hashed = hashlib.md5(hashIt.encode())
         return hashed.hexdigest()
@@ -12,27 +15,29 @@ def AlgorithmChoice(x):
         hashed = hashlib.sha512(hashIt.encode())
         return hashed.hexdigest()
 
-def CheckInput():
+def CheckInput(uInput):
     try:
-        testInput = int(userInput)
+        testInput = int(uInput)
         if testInput != 1 and testInput != 2 and testInput != 3:
             raise ValueError("Choose number 1, 2 or 3!!")
     except:
         raise TypeError("The input must be integer!!")
 
-def CheckLength():
-    if len(password) < 8:
+def CheckLength(passw, hsh):
+    if len(passw) < 8:
         raise ValueError("Password should have 8 symbols or more!!")
-    if len(hash) < 5:
+    if len(hsh) < 5:
         raise ValueError("Hash should have 5 symbols or more!!")
 
+def main():
+    password = input("Password to hash (8 or more symbols): ")
+    hash = input("Your own hash (5 or more symbols): ")
+    userInput = input("Choose algorith (1 = md5 ; 2 = sha384 ; 3 = sha512): ")
 
-password = input("Password to hash (8 or more symbols): ")
-hash = input("Your own hash (5 or more symbols): ")
-userInput = input("Choose algorith (1 = md5 ; 2 = sha384 ; 3 = sha512): ")
+    CheckInput(userInput)
+    CheckLength(password, hash)
 
-CheckInput()
-CheckLength()
+    print("-----------------------------------------------------------------------------------------------------------------------")
+    print("Hashed password: " + AlgorithmChoice(int(userInput), password, hash) + "\n")
 
-print("-----------------------------------------------------------------------------------------------------------------------")
-print("Hashed password: " + AlgorithmChoice(int(userInput)) + "\n")
+main()
