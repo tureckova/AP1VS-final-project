ed7eebd807bfb1c2be984a912aa545b89a01c2d1
Filip Hajduch <filip.hajduch03@gmail.com>
2022-12-07 08:24:16 +0100

    uprava main.py komentaru

--- main.py
+++ main.py
@@ -1,62 +1,125 @@
+"""
+Uvod.
 
+Závěrečný projekt předmětu Ap1VS
+.. include:: README.md
+Autori: Libor Vyleta, Filip Hajduch
+"""
 import math
 
 
 def vypocetstrany(bod1x, bod1y, bod2x, bod2y):
-   if type(bod1x) not in[int,float] or type(bod2x) not in[int,float] or type(bod1y) not in[int,float] or type(bod2y) not in[int,float]:
-       raise TypeError()
-   else:
-    a = bod2x - bod1x
-    b = bod2y - bod1y
-    strana = math.sqrt(math.pow(a, 2) + math.pow(b, 2))
-    return strana
+    """
+    Vypocet strany.
+
+                :param bod1x,bod1y,Bod2x,Bod2y:
+                Vstupní parametry pro vypocet stran
+                :return: Vrací stranu
+    """
+    if type(bod1x) not in [int, float] or type(bod2x) not in [int, float] or \
+            type(bod1y) not in [int, float] or type(
+            bod2y) not in [int, float]:
+        raise TypeError()
+    else:
+        a = bod2x - bod1x
+        b = bod2y - bod1y
+        strana = math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+        return strana
 
 
 def sestavitelnost(strana_a, strana_b, strana_c):
-    if type(strana_a) not in [int, float] or type(strana_b) not in [int, float] or type(strana_c) not in [int, float]:
+    """
+    Sestavitelnost.
+
+            :param strana_a, strana_b, strana_c:
+            Vstupní parametry pro sestavitelnost
+            :return: Vrací hodnotu True || False
+            trojuhelnik lze sestavit pokud platí všechny tyto pravidla
+    """
+    if type(strana_a) not in [int, float] or type(strana_b) \
+            not in [int, float] or type(strana_c) not in [int, float]:
         raise TypeError()
     else:
-        if strana_a+strana_b > strana_c or strana_a+strana_c > strana_b or strana_b+strana_c > strana_a:
+        if strana_a+strana_b > strana_c or strana_a+strana_c > strana_b or \
+                strana_b+strana_c > strana_a:
             return True
         else:
             return False
 
 
 def obsah(strana_a, strana_b, strana_c):
-    if type(strana_a) not in [int, float] or type(strana_b) not in [int, float] or type(strana_c) not in [int, float]:
+    """
+    Výpočet obsahu přes Heronův vzorec.
+
+                s -- potřebné pro výpočet obsahu obecného trojúhelníka
+                :param strana_a, strana_b, strana_c:
+                Vstupní parametry pro výpočet obsahu
+                :return: Vrací hodnotu obsahu
+    """
+    if type(strana_a) not in [int, float] or type(strana_b) not in \
+            [int, float] or type(strana_c) not in [int, float]:
         raise TypeError()
     else:
         s = (strana_a + strana_b + strana_c) / 2
-        obsah_s = math.sqrt(s * (s - strana_a) * (s - strana_b) * (s - strana_c))
+        obsah_s = math.sqrt(s * (s - strana_a) *
+                            (s - strana_b) * (s - strana_c))
         return obsah_s
 
 
 def obvod(strana_a, strana_b, strana_c):
-   if type(strana_a) not in[int,float] or type(strana_b) not in[int,float] or type(strana_c) not in[int,float]:
-       raise TypeError()
+    """
+    Výpočet obvodu.
 
-   else:
-    obvod_o = round(strana_a + strana_b + strana_c, 2)
-    return obvod_o
+                :param strana_a, strana_b, strana_c:
+                Vstupní parametry pro výpočet obvodu
+                :return: Vrací hodnotu obvodu
+    """
+    if type(strana_a) not in [int, float] or type(strana_b) not in \
+            [int, float] or type(strana_c) not in [int, float]:
+        raise TypeError()
+
+    else:
+        obvod_o = round(strana_a + strana_b + strana_c, 2)
+        return obvod_o
 
 
 def uhel(uhel_u_strany, strana_protilehla1, strana_protilehla2):
-   if type(uhel_u_strany) not in[int, float] or type(strana_protilehla1) not in[int, float] or type(strana_protilehla2) not in[int, float]:
+    """
+    Výpočet úhlů.
+
+                    :param uhel_u_strany, strana_protilehla1,
+                     strana_protilehla2:
+                    Vstupní parametry pro výpočet úhlů
+                    :return: Vrací hodnotu finalního úhlu
+    """
+    if type(uhel_u_strany) not in [int, float] or type(strana_protilehla1) \
+            not in [int, float] or type(
+            strana_protilehla2) not in [int, float]:
         raise TypeError()
 
-   else:
-        uhel1 = (math.pow(strana_protilehla1, 2) + math.pow(strana_protilehla2, 2) - math.pow(uhel_u_strany, 2)) / \
-            (2 * strana_protilehla1 * strana_protilehla2)
+    else:
+        uhel1 = (math.pow(strana_protilehla1, 2) +
+                 math.pow(strana_protilehla2, 2)
+                 - math.pow(uhel_u_strany, 2)) / \
+                (2 * strana_protilehla1 * strana_protilehla2)
         uhel_final = round(math.degrees(math.acos(uhel1)), 2)
         return uhel_final
 
 
 def pravouhlost(alfa, beta, gama):
+    """
+    Výpočet pravoúhlosti.
 
-   if type(alfa) not in[int,float] or type(beta) not in[int,float] or type(gama) not in[int,float]:
+    :param alfa: Vstupní parametr alfa.
+    :param beta: Vstupní parametr beta.
+    :param gama: Vstupní parametr gama.
+    :return:True|| False
+    """
+    if type(alfa) not in [int, float] or type(beta) not in [int, float] or \
+            type(gama) not in [int, float]:
         raise TypeError()
 
-   else:
+    else:
         if alfa == 90 or beta == 90 or gama == 90:
             return True
         else:
@@ -64,7 +127,7 @@ def pravouhlost(alfa, beta, gama):
 
 
 def triangle(x1, y1, x2, y2, x3, y3):
-
+    """Výpočet Trojúhelníku."""
     strana_a = vypocetstrany(x1, y1, x2, y2)
     strana_b = vypocetstrany(x2, y2, x3, y3)
     strana_c = vypocetstrany(x1, y1, x3, y3)
@@ -76,7 +139,7 @@ def triangle(x1, y1, x2, y2, x3, y3):
         uhel_beta = uhel(strana_b, strana_a, strana_c)
         uhel_gama = uhel(strana_c, strana_b, strana_a)
 
-        if pravouhlost(uhel_alfa,uhel_beta,uhel_gama):
+        if pravouhlost(uhel_alfa, uhel_beta, uhel_gama):
             print("Trojuhelnik je pravouhly")
         else:
             print("Trojuhelnik neni pravouhly")
