0d648a81fbe13d3b9f4ca2af4d0217a6ef7868f4
Lukáš Urban <lukas.urbann@seznam.cz>
2022-12-08 14:54:30 +0100

    I have made the infamous flake8 bow before me

--- urban/main.py
+++ urban/main.py
@@ -1,36 +1,47 @@
+"""TerminalHasher.
+
+A command-line utility for viewing cryptographic
+translations of user defined passwords.
+"""
+
+
 import hashlib
+"""Library with all the fancy hash functions"""
 
-"""
-A command-line utility for viewing cryptographic translations of user defined passwords.
-"""
 
-def EncryptPassword(passwd, type):
-    match type:
+def encrypt_password(password, hash_type):
+    """encrypt_password encrypts the given password and returns the hash."""
+    match hash_type:
         case "md5":
-            result = hashlib.md5(passwd.encode())
+            result = hashlib.md5(password.encode())
         case "sha1":
-            result = hashlib.sha1(passwd.encode())
+            result = hashlib.sha1(password.encode())
         case "sha256":
-            result = hashlib.sha256(passwd.encode())
+            result = hashlib.sha256(password.encode())
         case "sha512":
-            result = hashlib.sha512(passwd.encode())
+            result = hashlib.sha512(password.encode())
         case _:
             print("Funny non existent hash algorithm detected.")
             return
 
-    print("You've chosen " + type + " as your hash algorithm.")
+    print("You've chosen " + hash_type + " as your hash algorithm.")
     print("The hexadecimal value of this password is: " + result.hexdigest())
     print("Thanks, and don't forget to come again!")
 
 
-def TypePassword():
+def type_password():
+    """type_password gets the user's password."""
     print("Please, enter your password(I won't look):")
     passwdinput = input()
     return passwdinput
 
 
-def ChooseAlgorithm():
-    print("Please, choose your algorithm:\n[1 - SHA1 || 2 - SHA256 || 3 - SHA512 || 4 - MD5]\n")
+def choose_algorithm():
+    """Choose Algorithm returns chooses the hash algorithm."""
+    print(
+        "Please, choose your algorithm:\n"
+        "[1 - SHA1 || 2 - SHA256 || 3 - SHA512 || 4 - MD5]\n"
+    )
     count = input()
 
     typeos = ''
@@ -48,24 +59,29 @@ def ChooseAlgorithm():
             typeos = ''
 
     return typeos
+
+
 def get_input(text):
+    """get_input prints the user's input."""
     return input(text)
 
-def Intro():
-    """Prints the welcome string."""
+
+def intro():
+    """Intro prints the welcome string."""
     print("Welcome to the best password hasher 2022")
 
 
 if __name__ == '__main__':
-    Intro()
+    """Main function that runs everything."""
+    intro()
 
     passwd = ''
     algorithm = ''
 
     while passwd == '':
-        passwd = TypePassword()
+        passwd = type_password()
 
     while algorithm == '':
-        algorithm = ChooseAlgorithm()
+        algorithm = choose_algorithm()
 
-    EncryptPassword(passwd, algorithm)
+    encrypt_password(passwd, algorithm)
--- urban/test_main.py
+++ urban/test_main.py
@@ -1,35 +1,49 @@
-"""
-Unit test for main.py
+"""TerminalHasher Test File.
+
+A command-line utility for viewing cryptographic
+translations of user defined passwords.
 """
 
-from main import *
-import pytest
+
+from main import intro, type_password, encrypt_password, get_input
+"""Main file's functions."""
 
 
 def test_intro(capsys):
-    Intro()
+    """test_intro tests the intro method."""
+    intro()
     captured = capsys.readouterr()
     assert captured.out.strip() == "Welcome to the best password hasher 2022"
 
+
 def test_choose_algorithm():
-    out1 = TypePassword()
-    out2 = TypePassword()
+    """test_choose_algorithm tests the choose_algorithm method."""
+    out1 = type_password()
+    out2 = type_password()
     assert out1 == out2
+
+
 def test_type_password():
-    text1 = TypePassword()
-    text2 = TypePassword()
+    """test_type_password tests the type_password method."""
+    text1 = type_password()
+    text2 = type_password()
     assert text1 == text2
 
-def test_encrypt_password(password, type):
-    #case md5
-    EncryptPassword(password,type)
-    #case sha1
-    EncryptPassword(password, type)
-    #case sha256
-    EncryptPassword(password, type)
-    #case sha512
-    EncryptPassword(password, type)
+
+def test_encrypt_password(password, hash_type):
+    """test_encrypt_password tests the encrypt_password method."""
+    # case md5
+    encrypt_password(password, hash_type)
+    # case sha1
+    encrypt_password(password, hash_type)
+    # case sha256
+    encrypt_password(password, hash_type)
+    # case sha512
+    encrypt_password(password, hash_type)
+
+
 def test_get_input(capsys):
+    """test_get_input tests the get_input method."""
     text1 = print("bagr")
-    text2 = captured = capsys.readouterr()
+    text2 = capsys.readouterr()
     assert get_input(text1) == text2
