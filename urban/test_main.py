05efef43c0ddd090f10c49ec6d759d4f1bddfefa
Lukáš Urban <lukas.urbann@seznam.cz>
2022-12-10 18:20:23 +0100

    Fixed flake8
    all my homies hate flake8

--- urban/test_main.py
+++ urban/test_main.py
@@ -5,7 +5,8 @@ translations of user defined passwords.
 """
 
 
-from main import intro, type_password, encrypt_password, get_input, choose_algorithm
+from main import intro, type_password, encrypt_password,\
+    get_input, choose_algorithm
 """Main file's functions."""
 
 
@@ -25,10 +26,33 @@ def test_get_input():
 
 def test_encrypt_password():
     """test_encrypt_password tests the encrypt_password method."""
-    assert "275ce9b199a2cbc9587bc281ad3f9e1313a65805" in encrypt_password(get_input("heslo@123"), choose_algorithm("1"), True)
-    assert "b1c484b75a66c70fafd58e0a20c25cc005d85e60ff82a3a13a64e23847ff1340" in encrypt_password(get_input("Pudl42069"), choose_algorithm("2"), True)
-    assert "19437359820bc1c7dea5000a2ccde2b0cbd29155b7e4fca650d8acafb3215683bbdf80f0d46268a7e347256e24c710e8beaa8cd4e2958b374d33d2ceda8d6212" in encrypt_password(get_input("vodaJeMokra."), choose_algorithm("3"), True)
-    assert "4a9de83d3698498e4d29d9a31dcbdebc" in encrypt_password(get_input("imagineProlomenyAlgoritmus@&$~^ˇ~1234ASFN"), choose_algorithm("4"), True)
+    password_one = "heslo@123"
+    password_two = "Pudl42069"
+    password_three = "vodaJeMokra."
+    password_four = "imagineProlomenyAlgoritmus@&$~^ˇ~1234ASFN"
+
+    hash_one = encrypt_password(get_input(password_one),
+                                choose_algorithm("1"), True)
+    hash_two = encrypt_password(get_input(password_two),
+                                choose_algorithm("2"), True)
+    hash_three = encrypt_password(get_input(password_three),
+                                  choose_algorithm("3"), True)
+    hash_four = encrypt_password(get_input(password_four),
+                                 choose_algorithm("4"), True)
+
+    premade_hash_one = "275ce9b199a2cbc9587bc281ad3f9e1313a65805"
+    premade_hash_two = "b1c484b75a66c70fafd58e0a20c25cc005d85e60" \
+                       "ff82a3a13a64e23847ff1340"
+    premade_hash_three = "19437359820bc1c7dea5000a2ccde2b0cbd291" \
+                         "55b7e4fca650d8acafb3215683bbdf80f0d462" \
+                         "68a7e347256e24c710e8beaa8cd4e2958b374d" \
+                         "33d2ceda8d6212"
+    premade_hash_four = "4a9de83d3698498e4d29d9a31dcbdebc"
+
+    assert hash_one == premade_hash_one
+    assert hash_two == premade_hash_two
+    assert hash_three == premade_hash_three
+    assert hash_four == premade_hash_four
 
 
 def test_type_password():
@@ -39,6 +63,7 @@ def test_type_password():
 
 
 def test_choose_algorithm():
+    """test_choose_algorithm tests the choose_algorithm method."""
     algo1 = choose_algorithm("1")
     algo2 = choose_algorithm("2")
     algo3 = choose_algorithm("3")
