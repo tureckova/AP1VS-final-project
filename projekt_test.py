885bb5950e340aff996cb94825d7a251ef956d1b
lukasdrdla <67752741+lukasdrdla@users.noreply.github.com>
2022-12-11 13:28:23 +0100

    Update projekt_test.py

--- projekt_test.py
+++ projekt_test.py
@@ -1,4 +1,4 @@
-from projekt import speak
+from projekt import speak, generateJoke
 import pytest
 
 def test_speak():
@@ -6,3 +6,13 @@ def test_speak():
     with pytest.raises(TypeError):
         speak(True)
         speak(int)
+
+def test_generateJoke():
+    # TypeError if needed
+    with pytest.raises(TypeError):
+        generateJoke(True)
+    # test for userInput
+    assert type(generateJoke(1)) == str
+    # test if word "Chuck" is in GenerateJoke(2)
+    word = "Chuck"
+    assert word in generateJoke(2)
