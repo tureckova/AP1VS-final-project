fb083e990cccdc343c93d5152673a7d2c00efbef
AshHydra <ash3152003@gmail.com>
2022-12-11 20:53:24 +0100

    msg

--- example_test.py
+++ example_test.py
@@ -4,24 +4,23 @@ import pytest
 
 
 def test_coding_edge_cases():
-    assert coding('HELLO') == '.... . .-.. .-.. --- '
-    assert coding('Hello,World') == '.... . .-.. .-.. ---  -- .-. .. ... - / .-- .-. .. -. ..-. '
-    assert coding('123') == '1 2 3 '
-    assert coding('HELLO world') == '.... . .-.. .-.. ---  .-- .-. .. -. ..-. '
-    assert coding(' ') == KeyError
-    assert coding(',') == KeyError  
+    assert coding('HELLO') == '.... . .-.. .-.. ---'
+    assert coding('Hello,World') == KeyError
+    assert coding('1') == KeyError
+    assert coding('HELLO world') == '.... . .-.. .-.. ---  .-- --- .-. .-.. -..'
+    
 test_coding_edge_cases()
 
-#To write unit tests for the decoding() function, we can test that it decodes strings correctly by comparing the output of the function with the expected result. For example:
+
 
 
 
 def test_decoding_edge_cases():
     assert decoding('.... . .-.. .-.. ---') == 'HELLO'
-    assert decoding('.-- .-. .. -. ..-. ') == 'WORLD'
-    assert decoding('.... . .-.. .-.. ---  .-- .-. .. -. ..-. ') == 'HELLO WORLD'
-    assert decoding('.... . .-.. .-.. ---  -- .-. .. ... - / .-- .-. .. -. ..-. ') == 'HELLO, WORLD!'
-    assert decoding('1 2 3 ') == '123'
-    assert decoding(' ') == KeyError
+    assert decoding('.-- --- .-. .-.. -..') == 'WORLD'
+    assert decoding('.... . .-.. .-.. ---  .-- --- .-. .-.. -..') == 'HELLO WORLD'
+    assert decoding('.... . .-.. .-.. ---  -- .-. .. ... - / .-- .-. .. -. ..-.') == KeyError
+    assert decoding('1 2 3 ') == KeyError
+    
 
 test_decoding_edge_cases()
