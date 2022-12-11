84fd3f3901c3e499f10f74a01ba2bb89ecbe158e
PaveLMezik <114338382+PaveLMezik@users.noreply.github.com>
2022-12-11 13:21:36 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -8,36 +8,29 @@ def speak(joke):
     computer.say(joke)
     computer.runAndWait()
 
+def generateJoke(userInput):
+    if userInput == 1:
+        joke = pyjokes.get_joke(category="all")
+    elif userInput == 2:
+        joke = pyjokes.get_joke(category="chuck")
+    elif userInput == 3:
+        joke = pyjokes.get_joke(category="neutral")
+    else:
+        print("nezadal jsi číslo v rozmezí 1 - 3! ")
+    return joke
 def main(game):
     while game:
         print("jakou kategorii vtipů chceš ?")
         print(" 1 - all \n" + " 2 - chuck\n" + " 3 - neutral")
         userInput = int(input())
-
-        if userInput == 1:
-            joke = pyjokes.get_joke(category="all")
-            speak(joke)
-            uI = input("další vtip ?  press any key => ANO, press N => NE\n")
-            if uI.upper() == "N":
-                game = False
-
-                
-        elif userInput == 2:
-            joke = pyjokes.get_joke(category="chuck")
-            speak(joke)
-            uI = input("další vtip ?  press any key => ANO, press N => NE\n")
-            if uI.upper() == "N":
-                game = False
-        elif userInput == 3:
-            joke = pyjokes.get_joke(category="neutral")
-            speak(joke)
-            uI = input("další vtip ?  press any key => ANO, press N => NE\n")
-            if uI.upper() == "N":
-                game = False
-        else:
-            print("nezadal jsi číslo v rozmezí 1 - 3! ")
+        joke = generateJoke(userInput)
+        speak(joke)
+        uI = input("další vtip ?  press any key => ANO, press N => NE\n")
+        if uI.upper() == "N":
+            game = False
 
     print("Konec programu")
+
 
 if __name__ == "__main__":
     main(True)
