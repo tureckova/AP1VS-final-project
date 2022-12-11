5d0f5214a7085d5930fe8c0f99d0fa3aa2a7feb7
David Polastik <david.polastik@seznam.cz>
2022-12-11 22:12:55 +0100

    flake8

--- example.py
+++ example.py
@@ -1,65 +1,70 @@
-dictionary={'A':'.-', 'B':'-...',
-            'C':'-.-.', 'D':'-..', 'E':'.','F':'..-.', 'G':'--.', 'H':'....',
-            'I':'..', 'J':'.---', 'K':'-.-','L':'.-..', 'M':'--', 'N':'-.',
-            'O':'---', 'P':'.--.', 'Q':'--.-','R':'.-.', 'S':'...', 'T':'-',
-            'U':'..-', 'V':'...-', 'W':'.--','X':'-..-', 'Y':'-.--', 'Z':'--..',}
+"""Morse code."""
 
-dictionary2={'.-':'A','-...':'B','-.-.':'C','-..':'D', '.':'E','..-.':'F','--.':'G','....':'H',
-             '..':'I','.---':'J', '-.-':'K','.-..':'L', '--':'M', '-.':'N',
-             '---':'O', '.--.':'P', '--.-':'Q','.-.':'R', '...':'S', '-':'T',
-             '..-':'U', '...-':'V', '.--':'W','-..-':'X', '-.--':'Y', '--..':'Z', '':' ',}
+dictionary = {'A': '.-', 'B': '-...',
+              'C': '-.-.', 'D': '-..', 'E': '.',
+              'F': '..-.', 'G': '--.', 'H': '....',
+              'I': '..', 'J': '.---', 'K': '-.-',
+              'L': '.-..', 'M': '--', 'N': '-.',
+              'O': '---', 'P': '.--.', 'Q': '--.-',
+              'R': '.-.', 'S': '...', 'T': '-',
+              'U': '..-', 'V': '...-', 'W': '.--',
+              'X': '-..-', 'Y': '-.--', 'Z': '--..', }
+
+dictionary2 = {'.-': 'A', '-...': 'B', '-.-.': 'C',
+               '-..': 'D', '.': 'E', '..-.': 'F',
+               '--.': 'G', '....': 'H',
+               '..': 'I', '.---': 'J',
+               '-.-': 'K', '.-..': 'L',
+               '--': 'M', '-.': 'N',
+               '---': 'O', '.--.': 'P', '--.-': 'Q',
+               '.-.': 'R', '...': 'S', '-': 'T',
+               '..-': 'U', '...-': 'V', '.--': 'W',
+               '-..-': 'X', '-.--': 'Y', '--..': 'Z', '': ' ', }
 
 
 def coding(s):
-    void=""
-    for i in s: #indexování
-        if i != ' ':
-            void+=dictionary[i]+' '
-        else:
-            void +=' '
-    
-    print(void)
-
-    
-def decoding(s):
+    """Coding."""
     void = ""
-    splitstring=s.split("  ")
-    
-    splitstring = s.split(" ")#rozdělení podle mezer
-    #splitstring=splitstring.remove('')
+    for i in s:  # indexování
+        if i != ' ':
+            void += dictionary[i]+' '
+        else:
+            void += ' '
 
-    
-    for i in splitstring: #indexování
-        void += dictionary2[i]
-    
     print(void)
-    
 
+
+def decoding(s):
+    """Coding."""
+    void = ""
+    splitstring = s.split("  ")
+    splitstring = s.split(" ")  # rozdělení podle mezer
+
+    for i in splitstring:  # indexování
+        void += dictionary2[i]
+
+    print(void)
 
 
 def selection(f):
-     f=int(input("1. Z ČEŠTINY DO MORSEOVKY || 2. Z MORESOVKY DO ČEŠTINY ")) # menu
-     return f
+    """Return the pathname of the KOS root directory."""
+    f = int(input("1.ČEŠTINA > MORSEOVKA || 2.MORESOVKA > ČEŠTINA "))  # menu
+    return f
+
 
 c = 1
 d = 0
-while(c!="0"):
-    d =selection(d)
-    a=input("ZADEJ TEXT NA ŠIFROVÁNÍ: ")
-    a=a.upper()
+while (c != "0"):
+    d = selection(d)
+    a = input("ZADEJ TEXT NA ŠIFROVÁNÍ: ")
+    a = a.upper()
 
-    startUp="" # nevyužívá se ale bez ní program nelze spustit
+    startUp = ""  # nevyužívá se ale bez ní program nelze spustit
 
-
-
-
-    if d==1: # šifrování
+    if d == 1:  # šifrování
         coding(a)
 
-
-
-    else: # dešifrování
+    else:  # dešifrování
         decoding(a)
 
-  
-    c = input(("POKUD CHCTE PROGRAM UKONČIT, ZADEJTE 0 || PRO POKRAČOVÁNÍ ZADEJTE LIBOVOLNÝ ZNAK : "))
+c = input(("PROGRAM SE UKONČI=0 || POKRAČOVÁNÍ=LIBOVOLNÝ ZNAK : "))
