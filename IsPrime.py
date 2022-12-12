02ef5fdd221c77a76589ac15392e1eeef6f35fe2
JustJirka <41378892+JustJirka@users.noreply.github.com>
2022-12-12 11:23:28 +0100

    added the TestPrvociselnosti function

---
+++ IsPrime.py
@@ -0,0 +1,26 @@
+import random
+def TestPrvociselnosti(x):
+    if x == 2:
+        return "Jedná se o prvočíslo."
+    if x % 2 == 0 or x <= 1:
+        return "Nejedná se o prvočíslo."
+    s = 0
+    d = x - 1
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+    for i in range(10):
+        a = random.randrange(2, x - 1)
+        x0 = pow(a, d, x)
+        if x0 == 1 or x0 == x - 1:
+            continue
+        for r in range(1, s):
+            x0 = pow(x0, 2, x)
+            if x0 == 1:
+                return "Nejedná se o prvočíslo."
+            if x0 == x - 1:
+                a = 0
+                break
+        if a:
+            return "Nejedná se o prvočíslo."
+    return "Jedná se o prvočíslo."
