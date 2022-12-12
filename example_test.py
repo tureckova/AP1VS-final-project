932ebef38e622b1ba2d2eaf2a666dfb787cd8296
David Polastik <david.polastik@seznam.cz>
2022-12-12 14:03:10 +0100

    uprava

--- example_test.py
+++ example_test.py
@@ -4,13 +4,13 @@ import pytest
 
 
 def test_coding():
-    assert test_coding('A') == '.-'
-    assert test_coding('B') == '-...'
+    assert coding('HELLO') == '.... . .-.. .-.. ---'
+    assert coding('WORLD') == '.-- --- .-. .-.. -..'
     
 
 def test_decoding():
-    assert test_decoding('.-') == 'A'
-    assert test_decoding('-...') == 'B'
+    assert decoding('.... . .-.. .-.. ---') == 'HELLO'
+    assert decoding('.-- --- .-. .-.. -..') == 'WORLD'
 
     
 if __name__ == "__main__":
