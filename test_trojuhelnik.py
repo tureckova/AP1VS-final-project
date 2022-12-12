46a49f6843f7a03aba08f5ae76530e97578cbf4a
Zurek Martin <118623965+ZurekMartin@users.noreply.github.com>
2022-12-12 21:37:04 +0100

    Update test_trojuhelnik.py
    Flake8 úpravy

--- test_trojuhelnik.py
+++ test_trojuhelnik.py
@@ -1,5 +1,6 @@
 """
 Závěrečný projekt z předmětu AP1VS.
+
 Skupina: ST1416
 Projekt: Trojúhelník
 Autoři: Martin Žůrek, David Fiala, David Tomeček, Josef Kužel, David Žídek
@@ -26,17 +27,17 @@ def test_delkaStrany():
 def test_obvod():
     """Test na výpočet obvodu trojúhelníku ABC."""
     assert obvod(7, 5, 4) == 16
-    
+
     """Zobrazí uživateli na konzoli hlášku TypeError"""
     with pytest.raises(TypeError):
         obvod(2 + 6j, 1 + 4j, 1 + 6j)
         obvod("o", "g", "a")
 
-    
+
 def test_obsah():
     """Test na výpočet obsahu trojúhelníku ABC."""
     assert obsah(8, 3, 6) == 7.644442425710328
-    
+
     """Zobrazí uživateli na konzoli hlášku TypeError"""
     with pytest.raises(TypeError):
         obsah(1 + 6j, 3 + 9j, 3 + 7j)
@@ -47,7 +48,7 @@ def test_sestrojitelnost():
     """Test na ověření sestrojitelnosti trojúhelníku ABC."""
     assert sestrojitelnost(6, 8, 9) is True
     assert sestrojitelnost(2, 2, 4) is False
-    
+
     """Zobrazí uživateli na konzoli hlášku TypeError"""
     with pytest.raises(TypeError):
         sestrojitelnost(9 + 6j, 8 + 2j, 6 + 7j)
@@ -58,7 +59,7 @@ def test_pravouhlost():
     """Test na ověření pravoúhlosti  trojúhelníku ABC."""
     assert pravouhlost(10, 8, 6) is True
     assert pravouhlost(4, 6, 8) is False
-    
+
     """Zobrazí uživateli na konzoli hlášku TypeError"""
     with pytest.raises(TypeError):
         pravouhlost(7 + 1j, 9 + 7j, 10 + 4j)
