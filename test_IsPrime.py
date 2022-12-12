e777d5aa9f7823ab1be35498a4ec0e312331d440
JustJirka <41378892+JustJirka@users.noreply.github.com>
2022-12-12 15:46:54 +0100

    made test_IsPrime.py flake8 compliant

--- test_IsPrime.py
+++ test_IsPrime.py
@@ -1,16 +1,18 @@
-"""Unit test for module IsPrime"""
+"""Unit test for module IsPrime."""
 from IsPrime import TestPrvociselnosti
 import pytest
 
+
 def test_TestPrvociselnosti():
-    # r>=0
+    """Test TestPrvociselnosti."""
+    """r>=0 """
     assert TestPrvociselnosti(7) == "Jedná se o prvočíslo."
-    # r<0
+    """ r<0 """
     assert TestPrvociselnosti(0) == "Nejedná se o prvočíslo."
     assert TestPrvociselnosti(-7) == "Nejedná se o prvočíslo."
     assert TestPrvociselnosti(-1) == "Nejedná se o prvočíslo."
 
-    # return TypeError in other cases
+    """ return TypeError in other cases """
     with pytest.raises(TypeError):
         TestPrvociselnosti(True)
     with pytest.raises(TypeError):
