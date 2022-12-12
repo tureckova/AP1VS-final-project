0f82cc14d6ce09164a53e51cb55a51062d54a06e
David Polastik <david.polastik@seznam.cz>
2022-12-12 13:29:01 +0100

    uprava

--- example_test.py
+++ example_test.py
@@ -5,17 +5,13 @@ import pytest
 
 def test_coding():
     assert coding('HELLO') == '.... . .-.. .-.. ---'
-    assert coding('Hello,World') == KeyError
-    assert coding('1') == KeyError
-    assert coding('HELLO world') == '.... . .-.. .-.. ---  .-- --- .-. .-.. -..'
+    assert coding('WORLD') == '.-- --- .-. .-.. -..'
     
 
 def test_decoding():
     assert decoding('.... . .-.. .-.. ---') == 'HELLO'
     assert decoding('.-- --- .-. .-.. -..') == 'WORLD'
-    assert decoding('.... . .-.. .-.. ---  .-- --- .-. .-.. -..') == 'HELLO WORLD'
-    assert decoding('.... . .-.. .-.. ---  -- .-. .. ... - / .-- .-. .. -. ..-.') == KeyError
-    assert decoding('1 2 3 ') == KeyError
+
     
 if __name__ == "__main__":
     pytest.main()
