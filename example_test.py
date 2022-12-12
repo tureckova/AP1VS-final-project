4e575a795cbf36a5c8be9cabdab6c2865c98808d
David Polastik <david.polastik@seznam.cz>
2022-12-12 12:03:42 +0100

    uprava

--- example_test.py
+++ example_test.py
@@ -3,27 +3,19 @@ from example import decoding
 import pytest
 
 
-def test_coding_edge_cases():
+def test_coding():
     assert coding('HELLO') == '.... . .-.. .-.. ---'
     assert coding('Hello,World') == KeyError
     assert coding('1') == KeyError
     assert coding('HELLO world') == '.... . .-.. .-.. ---  .-- --- .-. .-.. -..'
     
-test_coding_edge_cases()
 
-
-
-
-
-def test_decoding_edge_cases():
+def test_decoding():
     assert decoding('.... . .-.. .-.. ---') == 'HELLO'
     assert decoding('.-- --- .-. .-.. -..') == 'WORLD'
     assert decoding('.... . .-.. .-.. ---  .-- --- .-. .-.. -..') == 'HELLO WORLD'
     assert decoding('.... . .-.. .-.. ---  -- .-. .. ... - / .-- .-. .. -. ..-.') == KeyError
     assert decoding('1 2 3 ') == KeyError
     
-
-test_decoding_edge_cases()
-
 if __name__ == "__main__":
     pytest.main()
