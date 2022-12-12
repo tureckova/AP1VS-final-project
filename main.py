54e21ed548c0473bdc0b76c667828219fe939642
Josef Kužel <119503297+eeagle9@users.noreply.github.com>
2022-12-12 11:17:43 +0100

    Update main.py
    Úprava kódu

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ Projekt: Trojuhelnik
 Autori: Martin Žůrek, David Fiala, David Tomeček, Josef Kužel, David Žídek
  .. include:: README.md
 """
-#Importuje knihovnu pro umožnění uspání kódu na určitý čas
 import time
 
-#V následujícím bloku zadá uživatel souřadnice pro body A, B, C
-#nadefinování proměnných pro jednotlivé body a výpis dotazu na zadání hodnot uživatelem
+
 aX = float(input("Zadejte souřadnici x pro bod A: "))
 aY = float(input("Zadejte souřadnici y pro bod A: "))
 bX = float(input("Zadejte souřadnici x pro bod B: "))
@@ -17,7 +15,7 @@ bY = float(input("Zadejte souřadnici y pro bod B: "))
 cX = float(input("Zadejte souřadnici x pro bod C: "))
 cY = float(input("Zadejte souřadnici y pro bod C: "))
 
-#Zadané souřadnice bodů se uživateli vypíší na konzoli
+
 print()
 print("Bod A = [%.1f, %.1f]" %(aX, aY))
 print("Bod B = [%.1f, %.1f]" %(bX, bY))
@@ -26,13 +24,13 @@ print("Bod C = [%.1f, %.1f]" %(cX, cY))
 time.sleep(2) #waitTime 2 sekundy
 print( )
 
-#V tomto bloku se uživateli ukáží vypočítané strany troujúhelníku
+
 print("Výpočet délky jednotlivých stran trojúhelníku ABC:")
-#početní operace pro výpočet délky každé strany strojúhelníku
+
 stranaA = numpy.sqrt(pow(bX - aX, 2) + pow(bY - aY, 2))
 stranaB = numpy.sqrt(pow(cX - bX, 2) + pow(cY - bY, 2))
 stranaC = numpy.sqrt(pow(aX - cX, 2) + pow(aY - cY, 2))
-#výpis vypočtených délek stran
+
 print("Délka strany a = %.1f cm." %stranaA)
 print("Délka strany b = %.1f cm." %stranaB)
 print("Délka strany c = %.1f cm." %stranaC)
@@ -40,30 +38,30 @@ print("Délka strany c = %.1f cm." %stranaC)
 time.sleep(2) #waitTime 2 sekundy
 print( )
 
-#V tomto bloku se ukáže vypočítaný obvod troujúhelníku
+
 print("Výpočet obvodu trojúhelníku ABC:")
-#vzorec pro výpočet obvodu trojúhelníku
+
 obvod = stranaA + stranaB + stranaC
-#výpis výsledku (hodnoty) obvodu
+
 print("Trojúhelník ABC má obvod = %.1f cm." %obvod)
 
 time.sleep(2) #waitTime 2 sekundy
 print( )
 
-#V tomto bloku se uživateli ukáže vypočítaný obsah zadaného trojúhelníku
+
 print("Výpočet obsahu trojúhelníku ABC:")
-#vzorec pro výpočet obsahu trojúhelníku
+
 s = (stranaA + stranaB + stranaC) / 2
 obsah = numpy.sqrt(s * (s - stranaA) * (s - stranaB) * (s - stranaC))
-#výpis velikosti obsahu
+
 print("Trojúhelník ABC má obsah = %.1f cm2." %obsah)
 
 time.sleep(2) #waitTime 2 sekundy
 print( )
 
-#V tomto bloku se uživateli zobrazí zda je vůbec možné trojúhelník sestrojit
+
 print("Ověření sestrojitelnosti trojúhelníku ABC:")
-#podmínka IF se vzorci pro ověření zda-li lze trojúhelník ABC sestrojit
+
 if ((stranaA + stranaB > stranaC) and
     (stranaB + stranaC > stranaA) and
     (stranaC + stranaA > stranaB)):
@@ -74,9 +72,9 @@ else:
 time.sleep(2) #waitTime 2 sekundy
 print( )
 
-#Poslední věc co se uživateli ukáže je ověření, zda je trojúhelník pravoúhlý
+
 print("Ověření pravoúhlosti trojúhelníku ABC:")
-#podmínka IF se vzorci pro ověření pravoúhlosti trojúhelníku
+
 if ((pow(stranaA, 2) + pow(stranaB, 2) == pow(stranaC, 2)) or
     (pow(stranaB, 2) + pow(stranaC, 2) == pow(stranaA, 2)) or
     (pow(stranaC, 2) + pow(stranaA, 2) == pow(stranaB, 2))):
