60b96161efe49b0d5032c81409338846e1bb6c23
David Polastik <david.polastik@seznam.cz>
2022-12-12 16:16:54 +0100

    uprava

--- example_test.py
+++ example_test.py
@@ -1,16 +1,18 @@
+"""Coding."""
 from example import coding
 from example import decoding
 import pytest
 
 
 def test_decoding():
+    """Coding."""
     assert decoding('.... . .-.. .-.. ---') == ('HELLO')
-    
+
 
 def test_coding():
+    """Coding."""
     assert type(coding('HELLO')) == str
 
 
-    
 if __name__ == "__main__":
     pytest.main()
