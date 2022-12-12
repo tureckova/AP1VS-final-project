4c60e206a2d3a5f993258928423341f90c28f98b
JustJirka <41378892+JustJirka@users.noreply.github.com>
2022-12-12 11:34:59 +0100

    added unit tests

---
+++ test_IsPrime.py
@@ -0,0 +1,12 @@
+"""Unit test for module circle"""
+from IsPrime import TestPrvociselnosti
+import pytest
+
+def test_TestPrvociselnosti():
+    # r>=0
+    assert TestPrvociselnosti(7) == "Jedná se o prvočíslo."
+    assert TestPrvociselnosti(1.5) == "Nejedná se o prvočíslo."
+    # r<0
+    assert TestPrvociselnosti(0) == "Nejedná se o prvočíslo."
+    assert TestPrvociselnosti(-7) == "Nejedná se o prvočíslo."
+    assert TestPrvociselnosti(-1) == 0
