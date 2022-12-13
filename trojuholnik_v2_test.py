1d1604fed60bf4e3aa9b51ef17c224e48e723bac
sBucka <118976633+sBucka@users.noreply.github.com>
2022-12-13 21:33:40 +0100

    Oprava Pytesty
    Som tam nehal par chyb ale podarilo sa mi ich lokalizovat a exterminovat ;)

--- trojuholnik_v2_test.py
+++ trojuholnik_v2_test.py
@@ -2,13 +2,13 @@ import pytest
 from trojuholnik_v2 import *
 
 def test_obvodu():
-    assert obvod_Stran(2,2,2)== 6
+    assert obvod_Stran(2,2,2)== 6.0
     
 def test_Obsah():
-    assert Obsah_Trojuholnika(2,2,2)==1,732050807568877
+    assert Obsah_Trojuholnika(5,5,8)==12.0
     
 def test_Strana():
-    assert strana(1,2,2,1)==(round(((1-2)**2+(2-1)**2)**(1/2)),2) 
-
+    assert strana(1,2,2,1)==(((1-2)**2+(2-1)**2)**(1/2))
+        
 def test_Uhol():
     assert uhol(0,1,1,0,0,0)==90.00000000000001
