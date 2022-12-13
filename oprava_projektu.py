017b2cef97fbdfdf9382d477939a829b6c4ad223
swpv <swpv@email.cz>
2022-12-13 13:56:23 +0100

    flake-uprava kodu(vzdrazil)

--- oprava_projektu.py
+++ oprava_projektu.py
@@ -1,21 +1,17 @@
 """this program is counting characters in string."""
 from collections import Counter
-
-x=True
-while x==True:
+x = True
+while x is True:
     retezec = input("Zadejte retezec: ")
     if (retezec == ""):
         retezec = "banana"
     print("Tvoje zvolene slovo je:", retezec)
     counter = Counter(retezec)
     print("Tvoje slovo je dlouhe: ", len(retezec), "znaku")
-
     countermax = max(counter, key=counter.get)
     print("Nejcastejsi znak je:", countermax)
-
     countermin = min(counter, key=counter.get)
     print("Nejmene casty znak je:", countermin)
-
     sum = 0
     for i in counter:
         sum += counter[i]
@@ -23,6 +19,6 @@ while x==True:
     print("Cetnost jednotlivych znaku:", counter)
     continu = input("Pro pokracovani napiste dalsi: ")
     if continu == "dalsi":
-        x=True
+        x = True
     else:
-        x=False
+        x = False
