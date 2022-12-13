4d057b7580122654bfbb39f392930bd8f142fb25
N0L1CZE <118295166+N0L1CZE@users.noreply.github.com>
2022-12-13 18:36:29 +0100

    fixed test file

--- test_HashingSoftware.py
+++ test_HashingSoftware.py
@@ -1,24 +1,17 @@
-from HashingSoftware import HashPassw, CheckInput, CheckLenght
+from HashingSoftware import HashPassw, CheckInput, CheckLength
 import pytest
 
 def test_HashPassw():
-    # HashPassw for
     assert HashPassw(1, "TezkeHeslo123", "SuperKratkyHash") == '7e803bead7cfded9b43e1e5a9cfbf0fc'
-
+    assert HashPassw(2, "TezkeHeslo123", "SuperKratkyHash") == '32af788bf1f2d1b7c4fb039c5b3f81ae8081b63bdc426f53a149bd66fc0f9776d4b23b3f961c954140331b58ed8bf56e'
+    assert HashPassw(3, "TezkeHeslo123", "SuperKratkyHash") == 'de6d9d4c3726426b07eefe687a8ad0e9df4e42e979a40504c0e9350b698b6656364ecaf01e2ce3f5b55e79224d3eee575f9bf8c50bd113d33badc3643d776d68'
+    
+    
 def test_CheckInput():
-    # CheckInput for
-    assert CheckInput()
-    assert CheckInput()
-    assert CheckInput()
-
-    #TypeError if needed
     with pytest.raises(TypeError):
         CheckInput("string")
 
-    
-
 def test_CheckLength():
-    # TypeError if needed
     with pytest.raises(ValueError):
-        CheckLenght("asdf", "asdfasdf)
-        CheckLenght("asdfasdfsadf", "ASDFASDFASFD")
+        CheckLength("asdf", "asdfasdf")
+        CheckLength("asdfasdfsadf", "ASDFASDFASFD")
