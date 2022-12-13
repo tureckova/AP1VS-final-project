a1852c0a70b4aa0a6550455e9068e489bb9e0e9d
Matěj Bača <m_baca@ad.utb.cz>
2022-12-13 16:45:32 +0100

    pridany docstring

--- number_systems.py
+++ number_systems.py
@@ -2,7 +2,34 @@
 
 
 def number_system_converter(dec):
-    """Convert decimal int into binary, hexadecimal and octal systems."""
+    """Convert decimal int into binary, hexadecimal and octal systems.
+
+    :param dec: Parameter of the converter function (int)
+
+    Sample usage:
+    >>> number_system_converter(0)
+    Traceback (most recent call last):
+    ...
+    ValueError: Value error. Input must be a positive number.
+    >>> number_system_converter(-7)
+    Traceback (most recent call last):
+    ...
+    ValueError: Value error. Input must be a positive number.
+    >>> number_system_converter(13)
+    'bin: 0b1101, hex: 0xd, oct: 0o15'
+    >>> number_system_converter(True)
+    Traceback (most recent call last):
+    ...
+    TypeError: Type error. Input must be an integer.
+    >>> number_system_converter(25.67)
+    Traceback (most recent call last):
+    ...
+    TypeError: Type error. Input must be an integer.
+    >>> number_system_converter("abcd")
+    Traceback (most recent call last):
+    ...
+    TypeError: Type error. Input must be an integer.
+    """
     if type(dec) not in [int]:
         raise TypeError("Type error. Input must be an integer.")
     if dec <= 0:
