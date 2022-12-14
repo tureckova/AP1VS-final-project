50844678028c28c4bf8c1f2b520a97cd545b8ed2
TheSouky <119810819+TheSouky@users.noreply.github.com>
2022-12-14 09:24:08 +0100

    uprava unit test

--- example_test.py
+++ example_test.py
@@ -8,15 +8,16 @@ def test_decoding():
     """Test decoding."""
     assert decoding('...././.-../.-../---') == ('HELLO')
     assert decoding('-.../') == ('B ')
-    
-
+ 
 
 def test_coding():
     """Test coding."""
     assert type(coding('HELLO')) == str
-    assert coding('MAM RAD JABLKA') == (' --/.-/--//.-./.-/-..//.---/.-/-.../.-../-.-/.-/')
+    assert coding('MAM RAD JABLKA') == (
+        ' --/.-/--//.-./.-/-..//.---/.-/-.../.-../-.-/.-/')
     assert coding('B') == ' -.../'
 
 
 if __name__ == "__main__":
     pytest.main()
+
