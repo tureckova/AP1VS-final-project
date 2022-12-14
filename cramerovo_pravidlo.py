67c705a0aafc89c0214d13bde2e7dbc323a01ddc
borecondrej <120043172+borecondrej@users.noreply.github.com>
2022-12-14 14:47:56 +0100

    Update cramerovo_pravidlo.py

--- cramerovo_pravidlo.py
+++ cramerovo_pravidlo.py
@@ -13,17 +13,17 @@ def cramer_rule(a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
 
     Zadanie hodnot neznamych.
     :param a1: Vstupní parametr souřadnice a11,
-    param b1: Vstupní parametr souřadnice a12,
-    param c1: Vstupní parametr souřadnice a13,
-    param d1: Vstupní parametr,
-    param a2: Vstupní parametr souřadnice a21,
-    param b2: Vstupní parametr souřadnice a22,
-    param c2: Vstupní parametr souřadnice a23,
-    param d2: Vstupní parametr,
-    param a3: Vstupní parametr souřadnice a31,
-    param b3: Vstupní parametr souřadnice a32,
-    param c3: Vstupní parametr souřadnice a33,
-    param d3: Vstupní parametr
+    :param b1: Vstupní parametr souřadnice a12,
+    :param c1: Vstupní parametr souřadnice a13,
+    :param d1: Vstupní parametr,
+    :param a2: Vstupní parametr souřadnice a21,
+    :param b2: Vstupní parametr souřadnice a22,
+    :param c2: Vstupní parametr souřadnice a23,
+    :param d2: Vstupní parametr,
+    :param a3: Vstupní parametr souřadnice a31,
+    :param b3: Vstupní parametr souřadnice a32,
+    :param c3: Vstupní parametr souřadnice a33,
+    :param d3: Vstupní parametr
     """
     if (type(a1) not in [int, float] or
             type(b1) not in [int, float] or
