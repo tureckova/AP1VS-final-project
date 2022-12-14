17e6deeac69097efd49ae7a3c5a023a19ad58786
DanielHlavicka21 <119601746+DanielHlavicka21@users.noreply.github.com>
2022-12-14 11:04:00 +0100

    uprava kodu kvuli flake8

--- final/projekt_kluci.py
+++ final/projekt_kluci.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def retezec(retezec):
-    """Function that returns informations about given string"""
+    """Collect information about characters in string."""
     """příklady Funkce.
 
     Sample usage:
@@ -13,7 +13,6 @@ def retezec(retezec):
     CeTnost jednotlivych znaku je rovna
     Prumerna cetnost znaku je:  4.0
     Cetnost jednotlivych znaku: Counter({'b': 4, 'l': 4, 'a': 4})
-    
     >>>retezec(2)
     Traceback (most recent call last):
     ...
@@ -38,7 +37,7 @@ def retezec(retezec):
         """Raises error if input given by user is not string"""
         raise TypeError("Imput must be string.")
     if (retezec == ""):
-        """if input is string but has no characters,then the default string is banana"""
+        """if input is string but has no characters,then the default string"""
         retezec = "banana"
     print("Tvoje zvolene slovo je:", retezec)
     counter = Counter(retezec)
@@ -50,13 +49,13 @@ def retezec(retezec):
     countermin = min(counter, key=counter.get)
     """variable that stores the least frequent character"""
     if (countermax != countermin):
-        """if variables countermax and countermin are different, then it do this ↓"""
+        """if variables countermax/min are different, then it do this ↓"""
         print("Nejcastejsi znak je:", countermax)
         """this line prints most frequent character"""
         print("Nejmene casty znak je:", countermin)
         """this line prints least frequent character"""
     else:
-        """if variables countermax and countermin are equal, then it do this ↓"""
+        """if variables countermax/min are equal, then it do this ↓"""
         print("CeTnost jednotlivych znaku je rovna")
     sum = 0
     for i in counter:
@@ -67,4 +66,3 @@ def retezec(retezec):
     print("Cetnost jednotlivych znaku:", counter)
     """this line prints frequency of each character"""
     return len(retezec), countermax, countermin, sum/len(counter), counter
-
