646ab017c968f1d73203c9612e25d26daa74045c
polida <polida.danek@gmial.com>
2022-12-14 02:57:45 +0100

    uprava chyb

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from FunkcePole import FunkcePole
 f = FunkcePole()
 s = f.Sort()
 x = f.pole()
-
+print("Vase pole je:\n", x, "\n")
 
 nejmensi = f.minimum(x)
 nejvetsi = f.maximum(x)
+print("Nejmensi cislo je ",
+      nejmensi[0],
+      " a nachazi se na pozici ",
+      nejmensi[1])
+print("Nejvetsi cislo je ",
+      nejvetsi[0],
+      " a nachazi se na pozici ",
+      nejvetsi[1],
+      "\n")
 
 s.vyber_sortu(x)
