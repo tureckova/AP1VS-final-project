7160427ae3bc146bc45cda9231ec1d8c44d65894
borecondrej <120043172+borecondrej@users.noreply.github.com>
2022-12-14 14:10:34 +0100

    Update test_cramerovo_pravidlo.py

--- test_cramerovo_pravidlo.py
+++ test_cramerovo_pravidlo.py
@@ -10,6 +10,7 @@ def test_cramer_rule():
     assert cramer_rule(2, 4, 3, 9, 5, 8, 7, 8, 9, 8, 2, 9) == (-5, 8, -5)
     assert cramer_rule(1, 2, 5, 7, 12, 9, 6, 5, 15, 10, 11, 14) == (0, -1, 2)
     assert cramer_rule(3, 4, 5, 12, 9, 3, 4, 15, 8, 2, 6, 21) == (1, -1, 3)
+    assert cramer_rule(1, 3, 3, 3, 3, 8, 7, 8, 9, 2, 4, 9) == (1, 0, 0)
 
     with pytest.raises(TypeError):
         cramer_rule("s", "a", "l", "A", "s", "a", "S", "x", "s", "v", "q", "a")
