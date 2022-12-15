792686998549c57910de38565d5b43d46ef91792
Petr Přikryl <petrik.prikryl@gmail.com>
2022-12-15 21:05:38 +0100

    snad uz to pojede

--- RomanNumbers1.py
+++ RomanNumbers1.py
@@ -2,7 +2,7 @@
 
 
 def intToRoman(cislo):
-"""Vytvoříme platnou Římskou soustavu podle pořadí 0-9.
+    """Vytvoříme platnou Římskou soustavu podle pořadí 0-9.
 
 :param cislo: vstupni promena funkce cislo !=<0 cislo!>4000
 :return: vraci vysledek po prevodu promenne cislo na rimskou soustavu
