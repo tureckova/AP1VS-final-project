79cdbb270d6a3c8a8ab958de5d412b6d9828c68c
TheCrowd1357 <120518982+TheCrowd1357@users.noreply.github.com>
2022-12-15 02:18:53 +0100

    Did that not get through?

--- prvocislo-main/prvocislo_test.py
+++ prvocislo-main/prvocislo_test.py
@@ -1,9 +1,10 @@
 """Test module prvocislo."""
-from prvocislo import *
+from prvocislo import type_prime, counting_prime, power, is_prime
 import pytest
 
+
 def test_type_prime():
-  """Test function type_prime"""
+  """Test function type_prime."""
   assert type_prime(1) == False
   assert type_prime(5) == True
   assert type_prime(11.0) == True
@@ -15,7 +16,7 @@ def test_type_prime():
 
 
 def test_counting_prime():
-  """Test function counting_prime"""
+  """Test function counting_prime."""
   # test case r=0
   assert counting_prime(1) == False 
   assert counting_prime(2) == True
@@ -30,14 +31,14 @@ def test_counting_prime():
 
 
 def test_power():
-  """Test function power"""
+  """Test function power."""
   assert power(2,12) != 1
   assert power(2,13) == 1
   assert power(5,35) != 1
 
 
 def test_is_prime():
-  """Test function is_prime"""
+  """Test function is_prime."""
   assert is_prime(2,3) == True
   assert is_prime(6,3) == False
   assert is_prime(17,3) == True
